11cedaddd43d044866df6aa7bba8660ad34b35d7
thakurgit <prakashthakur@crescerance.com>
2022-04-29 23:22:06 +0530

    badge parent class

--- wp-content/plugins/engagifii/engagifii_css.php
+++ wp-content/plugins/engagifii/engagifii_css.php
@@ -400,11 +400,11 @@ $ebt_detail_calendar_strip_hover_color         = $settings['ebt_detail_calendar_
 .class-background{background-color: <?php echo $ebt_detail_class_list_background_color; ?>}
 .class-text span, .class-text div{color: <?php echo $ebt_detail_class_calendar_text_color; ?>}
 .calendar__day:not(.today):hover, .calendar__day.active{background-color: <?php echo $ebt_detail_calendar_hover_color; ?> !important;}
-.classNames:hover {background-color: <?php echo $ebt_detail_calendar_strip_hover_color; ?>; }
+.classNames > a:hover {background-color: <?php echo $ebt_detail_calendar_strip_hover_color; ?>; }
 /* CSS for Class Names on Calendar view */
 
 
-.classNames {
+.classNames > a {
     background-color: <?php echo $ebt_detail_calendar_strip_color; ?> !important;
 }
 
