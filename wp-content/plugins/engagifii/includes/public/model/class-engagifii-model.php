a4ddb96415199d3ec29541a0fed904b4fe4ea482
thakurgit <prakashthakur@crescerance.com>
2022-05-04 12:09:17 +0530

    tooltip

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -238,14 +238,14 @@ class abstractModelEngagifii extends Engagifii_API
                       $nestedData['register'] = '<a href="'.$value->registrationUrlOnLine.'" id="onlineclass" class="btn btn-primary px-3 py-1" target="_blank" style="margin-top:1px; margin-bottom:1px; font-size:11px;">Register Online</a><br/><a href="'.$value->registrationUrlOnLocation.'" id="onlocation" class="btn btn-primary px-3 py-1" target="_blank" style="margin-top:1px; margin-bottom:1px; font-size:11px;">Register in person</a>';
                       }
                   else{
-                      $nestedData['register'] = '<a href="#" id="onlocation" class="btn btn-primary  px-3 py-1" data-toggle="tooltip" data-placement="right" title="Class Location not defined" disabled >Register</a>';
+                      $nestedData['register'] = '<button type="button" id="onlocation" class="btn btn-primary  px-3 py-1" data-toggle="tooltip" data-placement="right" title="Class Location not defined" disabled >Register</button>';
                   }
               }
               else{
-              $nestedData['register'] = '<a href="#" id="onlocation" class="btn btn-primary  px-3 py-1" data-toggle="tooltip" data-placement="right" title="'.$value->registrationState.'" >Register</a>';
+              $nestedData['register'] = '<button type="button" id="onlocation" class="btn btn-primary  px-3 py-1" data-toggle="tooltip" data-placement="right" title="'.$value->registrationState.'" >Register</button>';
               }
           }else{
-            $nestedData['register'] = '<a href="#" id="onlocation" class="btn btn-primary  px-3 py-1" data-toggle="tooltip" data-placement="right" title="'.$value->registrationState.'" disabled >Register</a>';
+            $nestedData['register'] = '<button type="button" id="onlocation" class="btn btn-primary  px-3 py-1" data-toggle="tooltip" data-placement="right" title="'.$value->registrationState.'" disabled >Register</button>';
             //$data[] = $nestedData;    
           }
           $data[] = $nestedData;
