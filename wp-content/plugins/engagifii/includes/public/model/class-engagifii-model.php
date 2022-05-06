667a9b15d2217497c7f2c7a24e28b3d6d22ad471
thakurgit <prakashthakur@crescerance.com>
2022-05-06 11:40:20 +0530

    registration closed tooltip

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -2148,7 +2148,7 @@ class abstractModelEngagifii extends Engagifii_API
 
 		                }
 		                else{
-		                    $data['register'] = '<a href="#" id="onlocation" class="btn btn-primary px-3 py-1" data-toggle="tooltip" data-placement="right" title="'.$value->registrationState.'" >Register</a>';
+		                    $data['register'] = '<button type="button" id="onlocation" class="btn btn-primary px-3 py-1" data-toggle="tooltip" data-placement="top" title="'.$value->registrationState.'" disabled>Register</button>';
 		                }
 		                
 		            }
