9410d7cda52858823760ef0da8c38eaf16727436
thakurgit <prakashthakur@crescerance.com>
2022-05-13 00:42:30 +0530

    endorsement list

--- wp-content/plugins/engagifii/includes/public/views/endorsement/calendar.php
+++ wp-content/plugins/engagifii/includes/public/views/endorsement/calendar.php
@@ -242,7 +242,7 @@ div#calendar_filter {
                 success:function(html){
                     $('#'+target_div).html(html);
                     $('.calendar__days').hide();
-                    $('#'+calendar_view).addClass('bg-secondary');
+                     $('#'+calendar_view).addClass('bg-primary text-white');
                     var date = year+'-'+month+'-'+day;
                     if(calendar_view == 'day'){ $('#event_list').hide();}
                     getEvents(date);
@@ -391,7 +391,7 @@ div#calendar_filter {
                     $('#calendar_div').html(html);
                     
                     getEvents(today);
-           			$('button#month').addClass('bg-secondary');
+           			$('button#month').addClass('bg-primary text-white ');
            			$('#dayView').hide();
                 $('#weekView').hide();
 
