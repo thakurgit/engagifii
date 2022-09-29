6c6296a6c46a5a674a8f33234c9e09bb3ec96366
Guru0777 <gurpreetsingh@crescerance.com>
2022-09-29 10:30:15 +0530

    code revert for testing

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -1450,10 +1450,10 @@ wp_die();
             $classPopover      = '';
 
             $class_icon = $value->parentCourse->iconReference;
-            if($siteURL == "https://engagifiwebstg.wpengine.com/oresa" || $siteURL == "https://engagifiiweb.com/oresa"){
-                $class_icon = ENGAGIFII_ASSETS_URL.'/images/oconee-logo.png';
+            // if($siteURL == "https://engagifiwebstg.wpengine.com/oresa" || $siteURL == "https://engagifiiweb.com/oresa"){
+            //     $class_icon = ENGAGIFII_ASSETS_URL.'/images/oconee-logo.png';
                 
-            }
+            // }
 
             if(count($value->classInstructors)){
                 $instructorPopOver = $this->_popOverInstructorData1($key, $value->classInstructors);
