2bb69944282c4d84cb2e3f00c39af13c9ed3ba51
Guru0777 <gurpreetsingh@crescerance.com>
2022-09-29 10:15:53 +0530

    test code push

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -230,10 +230,10 @@ public function calendar_mode(){
         foreach ($collection as $key => $value) {
             
             $class_icon = $value->parentCourse->iconReference;
-            if($siteURL == "https://localhost/engagifiwebstg" || $siteURL == "https://engagifiwebstg.wpengine.com/oresa" || $siteURL == "https://engagifiiweb.com/oresa"){
-                $class_icon = ENGAGIFII_ASSETS_URL.'/images/oconee-logo.png';
+            // if($siteURL == "https://localhost/engagifiwebstg" || $siteURL == "https://engagifiwebstg.wpengine.com/oresa" || $siteURL == "https://engagifiiweb.com/oresa"){
+            //     $class_icon = ENGAGIFII_ASSETS_URL.'/images/oconee-logo.png';
                 
-            }
+            // }
         	// if(count($value->classSessions))
         	// {
         	// 	foreach ($value->classSessions as $index => $class) {
@@ -440,7 +440,7 @@ public function getCalendarClassName(){
                 $classdata = $this->classCalendar();
                 //echo "Hello Class Data";
 
-                print_r(json_encode($classdata));
+                //print_r(json_encode($classdata));
 
                 echo '<div class="calendar__week text-center d-flex justify-content-around border-top">';
                 for($cb=1;$cb<=$boxDisplay;$cb++){
@@ -782,7 +782,7 @@ public function getCalendarClassName1(){
                 $dayCount = 1;
                 $classdata = $this->classCalendar();
                 
-print_r(json_encode($classdata));
+//print_r(json_encode($classdata));
                 echo '<div class="calendar__week text-center d-flex justify-content-around border-top">';
                 for($cb=1;$cb<=$boxDisplay;$cb++){
                     if(($cb >= $currentMonthFirstDay || $currentMonthFirstDay == 1) && $cb <= ($totalDaysOfMonthDisplay)){
