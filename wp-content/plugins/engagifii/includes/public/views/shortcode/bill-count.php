3e3e552b19495fed9c6f1279535b7c089769c229
thakurgit <prakashthakur@crescerance.com>
2023-01-14 21:04:18 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/views/shortcode/bill-count.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/bill-count.php
@@ -6,7 +6,8 @@ $(document).ready(function(){
 	getCountSelected();
 	$('.session-tab li button').click(function(){
 		sessionId = $(this).attr('id');
-		alert(sessionId);
+		//alert(sessionId);
+		getCountSelected();
 	});
 });
 
@@ -16,7 +17,7 @@ function getCountSelected()
       type : "post",
       url: engagifiiUrl_ajaxurl,
       data:{
-		//sessionId = sessionId,
+		sessionId = sessionId,
         action:'legislationfiltercountdata'
       },
       success: function(response) {      
