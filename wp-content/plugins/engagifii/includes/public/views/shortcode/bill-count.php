06370e936abea65688c88343e43ff66274341ed4
thakurgit <prakashthakur@crescerance.com>
2023-01-16 14:01:00 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/views/shortcode/bill-count.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/bill-count.php
@@ -37,7 +37,7 @@ function getCountSelected()
          }
     });
 }
- $('body').on('click','.session-tab li:first-child button', function() {
-     $(this).trigger('click') 
+ $(window).on('load', function() {
+     $('.session-tab li:first-child button').trigger('click') 
 });
 </script>
