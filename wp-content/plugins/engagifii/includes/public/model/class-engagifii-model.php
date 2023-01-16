b3858877a0ee76560bb3d38a5ed294ce6bd9be42
thakurgit <prakashthakur@crescerance.com>
2023-01-16 18:10:15 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -1486,7 +1486,7 @@ wp_die();
 		$session = $postedData['sessionId'];
         $dataResponse = $this->submitApiRequest("legislative/public-bills/filter/billusers?sessionId=".$session,$postedData,"GET", 'legislation');
         header("Content-Type: application/json"); 
-		print_r($dataResponse);
+		//print_r($dataResponse);
 		//die;  
         echo json_encode($dataResponse);
         wp_die();
