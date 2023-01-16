7417fec93ef1d1ff2e4cc10f98ab8404880d9261
thakurgit <prakashthakur@crescerance.com>
2023-01-16 10:41:16 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/views/shortcode/sessions.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/sessions.php
@@ -42,7 +42,7 @@ $sessionResponses = json_decode($sessionResponse['api_response']);
     
 
   <script type="text/javascript">
-  $(window).onload(function() {
+  $(window).on('load', function() {
      $('.session-tab li:first-child button').trigger('click') 
 });
 
