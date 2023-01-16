e3a64436c75298dce2259ab1dc1acadc5c9a6780
thakurgit <prakashthakur@crescerance.com>
2023-01-16 17:21:43 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/views/shortcode/trackingLevel.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/trackingLevel.php
@@ -59,7 +59,7 @@
 			var html='';
 			$.each(data, function(i, item) {
 				if(item.count>0){
-				html +='<a href="<?php echo get_site_url(); ?>/bill-tracking/?tracking='+item.trackingLevelId+'&'+btoa(item.title)+'&sessionID='+sessionId+'"><div class="alert  regular d-fw mb-2 col-12 p-2" data-id="'+item.trackingLevelId+'" style="border: 2px solid '+item.colorCode+' !important;"><div class="d-flex align-items-center"><p class="text-dark">'+item.title+'</p></div></div></a>';
+				html +='<a href="<?php echo get_site_url(); ?>/bill-tracking/?tracking='+item.trackingLevelId+'&'+btoa(item.title)+'&sessionId='+sessionId+'"><div class="alert  regular d-fw mb-2 col-12 p-2" data-id="'+item.trackingLevelId+'" style="border: 2px solid '+item.colorCode+' !important;"><div class="d-flex align-items-center"><p class="text-dark">'+item.title+'</p></div></div></a>';
 			}
 			});			
 			
