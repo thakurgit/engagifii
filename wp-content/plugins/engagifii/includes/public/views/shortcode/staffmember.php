0910f258bcf6cf5588b6a96e261cb93966b2677f
thakurgit <prakashthakur@crescerance.com>
2023-01-17 12:51:09 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/views/shortcode/staffmember.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/staffmember.php
@@ -152,7 +152,7 @@ function getStaffMembers()
 			var allmembers = <?php echo $lbt_visib_members_list; ?>;
 			$.each(data, function(i, item) {
 				if(item.count>0){
-					if(jQuery.inArray(item.personId, allmembers) !== -1)
+					if(jQuery.inArray(item.personId, allmembers) !== -1){
 				 html +='<option data-title="'+btoa(item.fullName)+'" data-type="member" data-id="'+item.personId+'" onclick="filterStaff('+item.personId+')" >'+item.fullName+' ('+item.count+')</option>';
 				}}
 			});			
