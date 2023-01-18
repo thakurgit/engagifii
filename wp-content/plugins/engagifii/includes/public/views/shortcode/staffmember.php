db589e27962860fd9f8651097b60c43c4b9a6d04
thakurgit <prakashthakur@crescerance.com>
2023-01-18 20:13:43 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/views/shortcode/staffmember.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/staffmember.php
@@ -130,8 +130,9 @@ if(site_url() == 'http://engagifiiweb.com')
 
   <script type="text/javascript">
   var sessionId='';
-
+var allmembers = <?php echo json_encode( $lbt_visib_members_list);  ?>;
 	$('.session-tab li button').click(function(){
+var allmembers = <?php echo json_encode( $lbt_visib_members_list);  ?>;
 		$('<div class="d-flex justify-content-center issue-loader position-absolute w-100 h-100 align-items-center" style="background:rgba(255,255,255,0.6);"><div class="spinner-grow text-primary" role="status"> <span class="sr-only">Loading...</span></div></div>').insertBefore(".legis-members"); 
 		sessionId = $(this).attr('id');
 		getStaffMembers();
@@ -147,7 +148,6 @@ function getStaffMembers()
       },
       success: function(response) {    
 	  		var data = response.api_response;
-			var allmembers = <?php echo json_encode( $lbt_visib_members_list);  ?>;
 console.log(allmembers);
 			data = JSON.parse(data);
 			var html='';
