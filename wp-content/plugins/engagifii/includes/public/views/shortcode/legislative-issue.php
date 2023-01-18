5738203ddb7a8645cecc1f019c075a994b754fc9
thakurgit <prakashthakur@crescerance.com>
2023-01-18 20:45:37 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/views/shortcode/legislative-issue.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/legislative-issue.php
@@ -83,9 +83,8 @@ function getLegislativeIssues()
 			data = JSON.parse(data);
 			var html='';
 			$.each(data, function(i, item) {
-				if(item.count>0){
+				
 				 html +=' <option class="text-break pb-1" data-title="'+btoa(item.text)+'" data-id="'+item.tagId+'" onclick="filterIssues('+item.tagId+')">'+item.text+' ('+item.count+')';
-				}
 			});			
 			
         	$('.legis-issues').html(html);
