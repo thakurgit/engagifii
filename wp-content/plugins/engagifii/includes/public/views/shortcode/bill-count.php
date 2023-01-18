2b61627c4d90ba37a4262cf838079d742ddb2a25
thakurgit <prakashthakur@crescerance.com>
2023-01-18 22:41:22 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/views/shortcode/bill-count.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/bill-count.php
@@ -12,7 +12,9 @@ $(document).ready(function(){
 	viewAll = $('#bill-count').siblings('a').attr('href');
 	<?php if(count($sessionResponses )>1) { ?>
 		sessionId = $('.session-tab li:first-child button').attr('id');
-	<?php }?>
+	<?php } else {
+	localStorage.setItem("sessionname", '');	
+	}?>
 	getCountSelected();
 		$('.session-tab li button').click(function(){
 			sessionId = $(this).attr('id');
