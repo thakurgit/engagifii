472509fdd850af9cbde523a5195c811f46abe73f
thakurgit <prakashthakur@crescerance.com>
2023-01-19 12:40:44 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/views/shortcode/legislative-issue.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/legislative-issue.php
@@ -46,15 +46,20 @@ usort($tags, "sort_associative_array");
    <?php foreach($tags as $tag){
           if(in_array($tag->tagId, $lbt_visib_legislative_list)){
 				if($tag->count>0){
+					$cevent = 	'filterIssues('.$tag->tagId.')';
+				} else {
+					$cevent = '';
+				}
         ?>
-        <option class="text-break pb-1" data-title="<?php echo base64_encode($tag->text);?>" data-id="<?php echo $tag->tagId;?>" onclick="filterIssues('<?php echo $tag->tagId; ?>')">
+        <option class="text-break pb-1" data-title="<?php echo base64_encode($tag->text);?>" data-id="<?php echo $tag->tagId;?>" onclick="<?php echo $cevent; ?>">
         
         <?php  
 
          echo $tag->text.' ('.$tag->count.')';
       ?>
         </option>
-        <?php } } }?>
+        <?php //}
+		 } }?>
     </select>
     </div>
     </div>
