55cb2da75e6f7b6d90518cd93b2a3b075a528770
thakurgit <prakashthakur@crescerance.com>
2023-01-19 13:17:13 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/views/shortcode/staffmember.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/staffmember.php
@@ -155,7 +155,7 @@ console.log(allmembers);
 			$.each(data, function(i, item) {
 				if(item.count>0){
 					html +='<option data-title="'+btoa(item.fullName)+'" data-type="member" data-id="'+item.personId+'" onclick="filterStaff('+item.personId+')" >'+item.fullName+' ('+item.count+')</option>';
-					if ($.inArray(parseInt(item.personId), allmembers) != -1) {
+					if ($.inArray(toString(item.personId), allmembers) != -1) {
 						console.log(item.personId+' Yes');
 				 
 				}else {
