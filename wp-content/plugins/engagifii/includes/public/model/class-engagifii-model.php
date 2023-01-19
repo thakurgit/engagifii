975091b5844d9ae385f6b8215d054a6c1a9e27a6
thakurgit <prakashthakur@crescerance.com>
2023-01-19 20:06:51 +0530

    sessions

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -1503,8 +1503,8 @@ wp_die();
 		$session = $postedData['sessionId'];
         $dataResponse = $this->submitApiRequest("legislative/public-bills/lastactions?sessionId=".$session,$postedData,"GET", 'legislation');
         header("Content-Type: application/json"); 
-		print_r($dataResponse);
-		die;  
+		//print_r($dataResponse);
+		//die;  
         echo json_encode($dataResponse);
         wp_die();
     }
