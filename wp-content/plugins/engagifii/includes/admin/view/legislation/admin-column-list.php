dd3bd5f4b49a63d0fdb8f065759cbed75436db46
thakurgit <prakashthakur@crescerance.com>
2023-01-20 15:34:42 +0530

    sessions

--- wp-content/plugins/engagifii/includes/admin/view/legislation/admin-column-list.php
+++ wp-content/plugins/engagifii/includes/admin/view/legislation/admin-column-list.php
@@ -16,7 +16,7 @@
 	$sessionResponse = $obj->getSessionsData();
 		echo '<div class="engagifii-setting  accordion-content" style="display:none;">'; ?>
 		    <h3>Multiple Sessions</h3><hr>
-<?php  //print_r($sessionResponse); 
+<?php  print_r($sessionResponse); 
 		echo '<ul class="ebt-grid-column-list" id="sessionList" style="width:100%; display:block;">';
 			foreach (json_decode($sessionResponse) as $session) {
                     $checked = "";
