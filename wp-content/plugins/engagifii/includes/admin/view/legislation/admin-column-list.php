b2ada0d846ea15fb8981581b744c9620f2ab21e0
thakurgit <prakashthakur@crescerance.com>
2023-01-20 17:17:37 +0530

    sessions

--- wp-content/plugins/engagifii/includes/admin/view/legislation/admin-column-list.php
+++ wp-content/plugins/engagifii/includes/admin/view/legislation/admin-column-list.php
@@ -35,6 +35,7 @@ echo '<div style="padding-left:7px"> <input type="checkbox" name="ebt_api_settin
 			} else {
 				$sessionoption = 'display:none;';
 			}
+			echo $sessionoption;
 		echo '<ul class="ebt-grid-column-list" id="sessionList" style="width:100%; '.$sessionoption.'">';
 			foreach (json_decode($sessionResponse) as $session) {
                     $checked = "";
