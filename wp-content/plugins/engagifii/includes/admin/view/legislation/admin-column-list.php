bd7854efc51322a25f3229fc8efed82bca6162bf
thakurgit <prakashthakur@crescerance.com>
2023-01-20 14:56:26 +0530

    sessions

--- wp-content/plugins/engagifii/includes/admin/view/legislation/admin-column-list.php
+++ wp-content/plugins/engagifii/includes/admin/view/legislation/admin-column-list.php
@@ -15,7 +15,7 @@
 	$sessionResponse = $obj->getSessionsData();
 		echo '<div class="engagifii-setting  accordion-content" style="display:none;">'; ?>
 		    <h3>Multiple Sessions</h3><hr>
-<?php  print_r($sessionResponse); 
+<?php  //print_r($sessionResponse); 
 		echo '<ul class="ebt-grid-column-list" id="sessionList" style="width:100%; display:block;">';
 			foreach (json_decode($sessionResponse) as $key => $row) {
  		echo '<li style="width:31%; display:inline-block;"> <input id="'.$row->sessionId.'" class="session-'.$row->sessionId.'" type="checkbox" name="ebt_api_settings[lbt_visib_session_list][]" '.$checked.' value='.$row->sessionId.'><label for="'.$row->sessionId.'">'.$row->sessionName.'</label></li>'		;
