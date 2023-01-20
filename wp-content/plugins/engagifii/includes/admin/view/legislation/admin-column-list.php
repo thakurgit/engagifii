6fc808a19d711d8719a48d9c50b4790160eb0059
thakurgit <prakashthakur@crescerance.com>
2023-01-20 14:54:29 +0530

    sessions

--- wp-content/plugins/engagifii/includes/admin/view/legislation/admin-column-list.php
+++ wp-content/plugins/engagifii/includes/admin/view/legislation/admin-column-list.php
@@ -18,7 +18,7 @@
 <?php  print_r($sessionResponse); 
 		echo '<ul class="ebt-grid-column-list" id="sessionList" style="width:100%; display:block;">';
 			foreach (json_decode($sessionResponse) as $key => $row) {
-				echo '<li style="width:31%; display:inline-block;" id="'.$row->sessionId.'">'.$row->sessionName.'</li>';
+ 		echo '<li style="width:31%; display:inline-block;"> <input id="'.$row->sessionId.'" class="session-'.$row->sessionId.'" type="checkbox" name="ebt_api_settings[lbt_visib_session_list][]" '.$checked.' value='.$row->sessionId.'><label for="'.$row->sessionId.'">'.$row->sessionName.'</label></li>'		;
 			}
 		echo '</ul>';
 
