735defad70c837dc91046be80db013c3713ee1c6
thakurgit <prakashthakur@crescerance.com>
2023-02-03 14:37:33 +0530

    search table

--- wp-content/plugins/engagifii/includes/public/views/legislation/grid-listing.php
+++ wp-content/plugins/engagifii/includes/public/views/legislation/grid-listing.php
@@ -1358,30 +1358,36 @@ if(table_key){
   $('#ebtmaintable thead tr th:eq('+table_key+')').each( function (i) {
       var title = $(this).text();
       $(this).html( '<input type="text" placeholder="Eg: HB 0002 or SR 0980" class="form-control form-control-sm search-endorsement" id="bill_number" value="'+billNumber+'"/>' );
- 	
-      $( 'input', this ).on( 'keyup change', function () {
-		  
-		  var strs = this.value;
-if (/\d/.test(strs)) {
-   var number = strs.match(/\d+/)[0];
- if (number.length == 3) {
-    number = '0' + number;
-  } else if (number.length == 2) {
-    number = '00' + number;
-  }else if(number.length == 1) {
-    number = '000' + number;
-  }
- strs = strs.replace(/\d+/, ' '+number);
+function delay(callback, ms) {
+  var timer = 0;
+  return function() {
+    var context = this, args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      callback.apply(context, args);
+    }, ms || 0);
+  };
 }
-console.log(strs);
-
+  $( 'input', this ).keyup(delay(function (e) {
+		  var strs = this.value;
+		  if (/\d/.test(strs)) {
+			 var number = strs.match(/\d+/)[0];
+		   if (number.length == 3) {
+			  number = '0' + number;
+			} else if (number.length == 2) {
+			  number = '00' + number;
+			}else if(number.length == 1) {
+			  number = '000' + number;
+			}
+		   strs = strs.replace(/\d+/, ' '+number);
+		  }
+		  console.log(strs);
          if ( table.column(i).search() !== strs ) {
-            table
-            .column(i)
-            .search( strs )
-            .draw();
+            table.column(i).search( strs ).draw();
          }
-      });
+	}, 500));
+ 	
+	  
    });
 }
 
