35da45f6a7a6f1daa4c64b50bd4c99bb02edfd40
thakurgit <prakashthakur@crescerance.com>
2023-02-11 00:28:41 +0530

    sequence

--- wp-content/plugins/engagifii/includes/admin/view/admin-settings-form.php
+++ wp-content/plugins/engagifii/includes/admin/view/admin-settings-form.php
@@ -274,7 +274,7 @@ return false;
 	function dropped(){
 		lbt_col_order=[];
 		jQuery( "#legislationList li:not(.toggleAll)" ).each(function(){
-			jQuery(this).attr('data-current-order',jQuery(this).index()+1);
+			jQuery(this).attr('data-current-order',jQuery(this).index());
 			lbt_col_order.push(jQuery(this).attr('data-order'));
 			
 		});
