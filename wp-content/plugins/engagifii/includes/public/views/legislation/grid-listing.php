c9e64fed72a71ddb477abe6c564f5c9ed606326e
Guru0777 <gurpreetsingh@crescerance.com>
2023-06-01 12:18:40 +0530

    AASB - Print Tracked Bill button added

--- wp-content/plugins/engagifii/includes/public/views/legislation/grid-listing.php
+++ wp-content/plugins/engagifii/includes/public/views/legislation/grid-listing.php
@@ -115,12 +115,18 @@ $assignToTags = $obj->legislationAssignToTagFilter();
 
 <div class="container-fluid pb-4">
 <span class="total-bill-text"></span>
-<p class="lead text-center"><span class="bill-count"></span>&nbsp;&nbsp;<?php if($tenant_url =="aasb") { ?>
-<span class="export-report"><a href="https://aasb.engagifii.com/public/lbt-report/7550/pdf/export" target="_blank"><img src="<?php echo ENGAGIFII_ASSETS_URL; ?>/images/pdf.png" alt="Export PDF"></a></span>
-<?php } ?></p>
+<p class="lead text-center"><span class="bill-count"></span></p>
 
 <div class="row rowEngagii tz-Engagii-flex">
-<div class="col-12 text-center text-lg-right d-flex align-items-center justify-content-end">
+        <div class="col-12 text-center text-lg-right d-flex align-items-center justify-content-end">
+<!-- Show Print Tracked Bill Button only if Tenant Code = AASB -->     
+<?php if($tenant_url =="aasb") { ?><span class="col-12 text-lg-left" style="padding-left:5rem !important"><button class="export-report text-lg-left py-1" >
+    <a href="https://aasb.engagifii.com/public/lbt-report/7550/pdf/export" target="_blank" style="color:inherit; font-weight:100;">
+        <img src="<?php echo ENGAGIFII_ASSETS_URL; ?>/images/pdf-icon.png" width=20 height=20 alt="PDF Icon" class="pdf-icon">
+        Print tracked bills
+    </a>
+</button> </span><?php } ?>
+<!-- End here -->
    <div class="one-coloumnsEngagii"> 
       <input type="hidden" name="enga_custom_multi_filter" id="enga_custom_multi_filter" value="">
       <div class="containerEngagii filter-icon d-inline-flex align-items-center justify-content-center rounded-circle position-relative bg-light border">
