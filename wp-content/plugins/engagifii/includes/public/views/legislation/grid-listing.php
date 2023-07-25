0dc59e6584d372d574334604b1c9a0a2031b0e91
thakurgit <prakashthakur@crescerance.com>
2023-07-25 14:30:12 +0530

    select all bill

--- wp-content/plugins/engagifii/includes/public/views/legislation/grid-listing.php
+++ wp-content/plugins/engagifii/includes/public/views/legislation/grid-listing.php
@@ -1610,14 +1610,11 @@ $(".tz-selectAll").change(function () {
 	if($(this).is(':checked')){
 		//$(this).parent().siblings().addClass('liactive deftzselected');
 		//$(this).parent().siblings().find('input').prop('checked',true);
-		$(this).parent().siblings().each(function(){
+		$(this).parent().siblings().each(function(e){
 		  if(!$(this).find('input').is(':checked')){
 			$(this).trigger('click');
-			$(this).find('input').click(function(e){
-				console.log('xxx');
-				e.stopPropagation();
-			});
 		  }
+		  e.stopPropagation();
 		});
 	}else{
 		//$(this).parent().siblings().trigger('click');
@@ -1627,6 +1624,7 @@ $(".tz-selectAll").change(function () {
 		  if($(this).find('input').is(':checked')){
 			$(this).trigger('click');
 		  }
+		  e.stopPropagation();
 		});
 	}
 });
