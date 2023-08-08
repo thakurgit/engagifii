f2f895e700ce9f35c535be9cf0ac5660641258fd
thakurgit <prakashthakur@crescerance.com>
2023-08-08 16:25:52 +0530

    profile menu

--- wp-content/plugins/engagifii/includes/admin/view/engagifii-profile.php
+++ wp-content/plugins/engagifii/includes/admin/view/engagifii-profile.php
@@ -1,49 +1,152 @@
-<style>
-.profile-wrapper * {
-	box-sizing: border-box;
-}
-.profile-wrapper img {
-max-width: 100%;
-height: auto;	
-}
-	.profile-header {
-	text-align: center;	
-	font-size: 32px;
-	}
-	.profile-body {
-	display: flex;	
-	justify-content:space-between;
-	width: 100%;
-	padding-right: 15px;
-	}
-	.left-panel {
-	flex: 0 0 33%;
-	max-width:33%;	
-	}
-	.right-panel {
-	flex: 0 0 65%;
-	max-width:65%;	
-	}
-.left-panel, .right-panel {
-	background: white;
-	-webkit-border-radius: 6px;
-	-moz-border-radius: 6px;
-	border-radius: 6px;
-	box-shadow: 0 0 10px rgba(0,0,0,0.1);
-	padding: 15px;
-}
-.left-panel {
-text-align: center;	
-}
-</style>
-<div class="profile-wrapper">
-<div class="profile-header">
-	<h2 >My Profile</h2>
-</div>
- <div class="profile-body">
- 	<div class="left-panel">
-    	<img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="">
+
+  <!-- Bootstrap CSS CDN link -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+<?php
+ ?>
+<section style="background-color: #eee;">
+  <div class="container py-5">
+    <div class="row">
+      <div class="col">
+        <h3>My Profile</h3>
+      </div>
     </div>
-    <div class="right-panel"></div>
- </div>   
-</div>
+
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="card mb-4">
+          <div class="card-body text-center">
+            <img src="http://www.accg.org/Staff%20Photos/Abercrombie_Ashley.jpg" alt="avatar"
+              class="rounded-circle img-fluid" style="width: 150px;">
+            <h5 class="my-3">Ms. Ashley Abercrombie</h5>
+            <p class="text-muted mb-1"><strong>PID:</strong> AACR7391</p>
+            <p class="text-muted mb-4">191 Peachtree Street, Suite 700 Atlanta, GA 30303</p>
+            <div class="d-flex justify-content-center mb-2">
+              <button type="button" class="btn btn-primary">Edit Profile</button>
+              
+            </div>
+          </div>
+        </div>
+        <div class="card mb-4 mb-lg-0">
+          <div class="card-body p-0">
+            <ul class="list-group list-group-flush rounded-3">
+              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
+                <i class="fas fa-globe fa-lg text-warning"></i>
+                <p class="mb-0"><strong>Tags: </strong>ACCG: All Staff, Assigned to Certified: Co, eLiability Beat, eRisk Connection, eSafety Theme, Mailings: County Line e-n</p>
+              </li>
+              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
+                <i class="fab fa-github fa-lg" style="color: #333333;"></i>
+                <p class="mb-0">Status: <span style="color:green;">Active</span></p>
+              </li>
+              <!-- <li class="list-group-item d-flex justify-content-left align-items-center p-3">
+                <i class="fab fa-twitter fa-lg" style="color: #55acee;"></i>
+                <p class="mb-0">@mdbootstrap</p>
+              </li>
+              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
+                <i class="fab fa-instagram fa-lg" style="color: #ac2bac;"></i>
+                <p class="mb-0">mdbootstrap</p>
+              </li>
+              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
+                <i class="fab fa-facebook-f fa-lg" style="color: #3b5998;"></i>
+                <p class="mb-0">mdbootstrap</p>
+              </li> -->
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-8">
+        <div class="card mb-4">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Full Name</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">Ms. Ashley Abercrombie</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Email</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">aabercrombie@accg.org</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Phone</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">(404) 522-5022</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Mobile</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">(404) 522-5022</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Address</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">191 Peachtree Street, Suite 700
+Atlanta, GA 30303</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card mb-4 mb-md-0">
+              <div class="card-body">
+              <p class="mb-4"><span class="text-primary font-italic me-1"><img src="https://engagifiistorage.blob.core.windows.net/icons/organization/7ce64db3-50d7-4b6f-a57a-3e5cf2e2e4b5.png" alt="avatar"
+              class="rounded-circle img-fluid" style="width: 30px;">Organization</span> 
+                </p>
+                <p class="mb-1" style="font-size: .77rem;">Association County Commissioners Of Georgia</p>
+                
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Department: Other</p>
+                
+                <!-- <p class="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
+                
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
+                
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p> -->
+                <!-- https://engagifiistorage.blob.core.windows.net/icons/organization/7ce64db3-50d7-4b6f-a57a-3e5cf2e2e4b5.png -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card mb-4 mb-md-0">
+            <div class="card-body">
+                <p class="mb-4"><span class="text-primary font-italic me-1"><img src="https://engagifiistorage.blob.core.windows.net/icons/organization/7ce64db3-50d7-4b6f-a57a-3e5cf2e2e4b5.png" alt="avatar"
+              class="rounded-circle img-fluid" style="width: 30px;">Organization</span> 
+                </p>
+                <p class="mb-1" style="font-size: .77rem;">Association County Commissioners Of Georgia</p>
+                
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Department: Other</p>
+                
+                 <p class="mt-4 mb-1" style="font-size: .77rem;"><strong>Position: </strong>Director of Property & Liability Insurance Programs</p>
+                
+               <!-- <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
+                
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p> -->
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
