5d8c1124b327f24b687bcab21f5e1220c2fc05c8
thakurgit <prakashthakur@crescerance.com>
2023-08-09 12:04:45 +0530

    profile menu

--- wp-content/plugins/engagifii/includes/admin/view/engagifii-profile.php
+++ wp-content/plugins/engagifii/includes/admin/view/engagifii-profile.php
@@ -4,7 +4,101 @@
 
 <?php
  ?>
-<section style="background-color: #eee;">
+ <div class="pe-3 mb-3">
+ 	<div class="container-fluid  ">
+    	<div class="row justify-content-end text-secondary small">
+        	<div class="col-md-3 d-flex align-items-center justify-content-end">
+            Select Profile Template: 
+            <select name="" id="tempswitch" class="form-control ms-2" style="width:130px">
+            	<option value="temp1">Template 1</option>
+                <option value="temp2">Template 2</option>
+                <option value="temp3">Template 3</option>
+            </select>
+            </div>
+            		
+        </div>
+    	
+    </div>
+ </div>
+ <section class="temp1 pe-3">
+ <div class="bg-light py-4 border-start border-4 border-primary mb-4">
+ 	<div class="container-fluid ">
+    	<div class="d-flex">
+  <div class="flex-shrink-0">
+  	<div class="overflow-hidden rounded-circle " style="width:120px;height:130px">
+    	<img src="http://www.accg.org/Staff%20Photos/Abercrombie_Ashley.jpg" alt="..." class="img-fluid">
+    </div>
+  </div>
+  <div class="flex-grow-1 ms-3">
+    <h3>Ms. Ashley Abercrombie</h3>
+    <p class="mb-0 text-secondary"><strong>PID:</strong> AACR7391</p>
+    <p class="text-secondary"><strong>Address: </strong>191 Peachtree Street, Suite 700 Atlanta, GA 30303</p>
+    <p class="text-secondary"><strong>Tags:</strong> <span class="badge rounded-pill text-bg-light border ms-2">Engagifii</span><span class="badge rounded-pill text-bg-light border ms-2">Legislation</span><span class="badge rounded-pill text-bg-light border ms-2">Bills</span><span class="badge rounded-pill text-bg-light border ms-2">Atlanta</span></p>
+  </div>
+</div>
+    </div>
+ </div>
+    <div class="container-fluid  bg-light border pt-2 pb-1">
+    	<h5>Contact Information</h5>
+    </div>
+    <div class="container-fluid mb-4 border py-3">
+    	<div class="row">
+        	<div class="col-md-6">
+            	<div class="card mb-4 px-2 m-0 bg-light mw-100">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0"><strong>Full Name</strong></p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">Ms. Ashley Abercrombie</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0"><strong>Email</strong></p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">aabercrombie@accg.org</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0"><strong>Phone</strong></p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">(404) 522-5022</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0"><strong>Mobile</strong></p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">(404) 522-5022</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0"><strong>Address</strong></p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">191 Peachtree Street, Suite 700
+Atlanta, GA 30303</p>
+              </div>
+            </div>
+          </div>
+        </div>
+            </div>
+        	<div class="col-md-6"></div>
+        </div>	
+    </div>
+    </section>
+<section class="temp2 pe-3"  style="background-color: #eee; display:none">
   <div class="container py-5">
     <div class="row">
       <div class="col">
@@ -148,5 +242,18 @@ Atlanta, GA 30303</p>
     </div>
   </div>
 </section>
-<?php ?>
+<script>
+jQuery(document).ready(function(){
+	var lasttemp =  localStorage.getItem("profile_template");
+	if(lasttemp){
+	jQuery('.'+lasttemp).show().siblings('section').hide();
+	jQuery('#tempswitch').val(lasttemp);
+	}
+});
+jQuery('#tempswitch').change(function(){
+		var tempid = jQuery(this).val();
+		jQuery('.'+tempid).show().siblings('section').hide();
+		 localStorage.setItem("profile_template", tempid);
+});
+</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
