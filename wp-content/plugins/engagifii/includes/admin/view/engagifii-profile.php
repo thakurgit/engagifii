d6e05bad5893cd5dd1d54d78e9b8e2a4df689a0b
thakurgit <prakashthakur@crescerance.com>
2023-08-10 16:39:46 +0530

    profile menu

--- wp-content/plugins/engagifii/includes/admin/view/engagifii-profile.php
+++ wp-content/plugins/engagifii/includes/admin/view/engagifii-profile.php
@@ -199,38 +199,44 @@ Atlanta, GA 30303</p>
 <section class="temp2 pe-3"  style="background-color: #eee; display:none"><div class="layout-content">
 
 <!-- Content -->
-<div class="container flex-grow-1 container-p-y">
 
   <!-- Header -->
-  <div class="container-m-nx container-m-ny theme-bg-white mb-4">
-    <div class="media col-md-10 col-lg-8 col-xl-7 py-5 mx-auto">
-      <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" class="d-block ui-w-100 rounded-circle">
-      <div class="media-body ml-5">
-        <h4 class="font-weight-bold mb-4">Nellie Maxwell</h4>
-
-        <div class="text-muted mb-4">
-          Lorem ipsum dolor sit amet, nibh suavitate qualisque ut nam. Ad harum primis electram duo, porro principes ei has.
-        </div>
-
-        <a href="javascript:void(0)" class="d-inline-block text-body">
-          <strong>234</strong>
-          <span class="text-muted">followers</span>
-        </a>
-        <a href="javascript:void(0)" class="d-inline-block text-body ml-3">
-          <strong>111</strong>
-          <span class="text-muted">following</span>
-        </a>
-      </div>
+  <div class="container-fluid bg-white mb-4 py-5">
+  <div class="row justify-content-center">
+  	<div class="col-md-7">
+    	<div class="d-flex">
+        	<div class="flex-shrink-0">
+  	<div class="overflow-hidden rounded-circle " style="width:120px;height:130px">
+    	<img src="http://www.accg.org/Staff%20Photos/Abercrombie_Ashley.jpg" alt="..." class="img-fluid">
     </div>
-    <hr class="m-0">
+  </div>
+  <div class="flex-grow-1 ms-3">
+  	<h4 class="font-weight-bold mb-4">Nellie Maxwell</h4>
+  
+          <div class="text-muted mb-4">
+            Lorem ipsum dolor sit amet, nibh suavitate qualisque ut nam. Ad harum primis electram duo, porro principes ei has.
+          </div>
+  
+          <a href="javascript:void(0)" class="d-inline-block text-body">
+            <strong>234</strong>
+            <span class="text-muted">followers</span>
+          </a>
+          <a href="javascript:void(0)" class="d-inline-block text-body ms-3">
+            <strong>111</strong>
+            <span class="text-muted">following</span>
+          </a>
+  </div>
+        </div>
+    </div>
+  </div>
   </div>
   <!-- Header -->
-
+<div class="container-fluid">
   <div class="row">
-    <div class="col">
+    <div class="col-8">
 
       <!-- Info -->
-      <div class="card mb-4">
+      <div class="card mb-4 px-3 mw-100">
         <div class="card-body">
 
           <div class="row mb-2">
@@ -312,10 +318,10 @@ Atlanta, GA 30303</p>
 
       
     </div>
-    <div class="col-xl-4">
+    <div class="col-4">
 
       <!-- Side info -->
-      <div class="card mb-4">
+      <div class="card mb-4 px-2 mw-100">
         <div class="card-body">
           <a href="javascript:void(0)" class="btn btn-primary rounded-pill">+&nbsp; Follow</a>
           &nbsp;
@@ -350,7 +356,7 @@ Atlanta, GA 30303</p>
       <!-- / Side info -->
 
       <!-- Skills -->
-      <div class="card mb-4">
+      <div class="card mb-4 px-2 mw-100">
         <div class="card-header">Skills</div>
         <div class="card-body">
 
@@ -389,6 +395,139 @@ Atlanta, GA 30303</p>
 
 
 </div>
+</section>
+	<section class="temp3 pe-3"  style="background-color: #eee; display:none">
+  <div class="container py-5">
+    <div class="row">
+      <div class="col">
+        <h3>My Profile</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="card mb-4">
+          <div class="card-body text-center">
+            <img src="http://www.accg.org/Staff%20Photos/Abercrombie_Ashley.jpg" alt="avatar"
+              class="rounded-circle img-fluid" style="width: 150px;">
+            <h5 class="my-3">Ms. Ashley Abercrombie</h5>
+            <p class="text-muted mb-1"><strong>PID:</strong> AACR7391</p>
+            <p class="text-muted mb-4">191 Peachtree Street, Suite 700 Atlanta, GA 30303</p>
+            <div class="d-flex justify-content-center mb-2">
+              <button type="button" class="btn btn-primary">Edit Profile</button>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-4 mb-lg-0">
+          <div class="card-body p-0">
+            <ul class="list-group list-group-flush rounded-3">
+              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
+                <i class="fas fa-globe fa-lg text-warning"></i>
+                <p class="mb-0"><strong>Tags: </strong>ACCG: All Staff, Assigned to Certified: Co, eLiability Beat, eRisk Connection, eSafety Theme, Mailings: County Line e-n</p>
+              </li>
+              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
+                <i class="fab fa-github fa-lg" style="color: #333333;"></i>
+                <p class="mb-0">Status: <span style="color:green;">Active</span></p>
+              </li>
+              <!-- <li class="list-group-item d-flex justify-content-left align-items-center p-3">
+                <i class="fab fa-twitter fa-lg" style="color: #55ACEE;"></i>
+                <p class="mb-0">@mdbootstrap</p>
+              </li>
+              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
+                <i class="fab fa-instagram fa-lg" style="color: #AC2BAC;"></i>
+                <p class="mb-0">mdbootstrap</p>
+              </li>
+              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
+                <i class="fab fa-facebook-f fa-lg" style="color: #3B5998;"></i>
+                <p class="mb-0">mdbootstrap</p>
+              </li> -->
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-8">
+        <div class="card mb-4">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Full Name</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">Ms. Ashley Abercrombie</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Email</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">aabercrombie@accg.org</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Phone</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">(404) 522-5022</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Mobile</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">(404) 522-5022</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Address</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">191 Peachtree Street, Suite 700
+Atlanta, GA 30303</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card mb-4 mb-md-0">
+              <div class="card-body">
+              <p class="mb-4"><span class="text-primary font-italic me-1"><img src="https://engagifiistorage.blob.core.windows.net/icons/organization/7ce64db3-50d7-4b6f-a57a-3e5cf2e2e4b5.png" alt="avatar"
+              class="rounded-circle img-fluid" style="width: 30px;">Organization</span>
+                </p>
+                <p class="mb-1" style="font-size: .77rem;">Association County Commissioners Of Georgia</p>
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Department: Other</p>
+                <!-- <p class="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p> -->
+                <!-- https://engagifiistorage.blob.core.windows.net/icons/organization/7ce64db3-50d7-4b6f-a57a-3e5cf2e2e4b5.png -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card mb-4 mb-md-0">
+            <div class="card-body">
+                <p class="mb-4"><span class="text-primary font-italic me-1"><img src="https://engagifiistorage.blob.core.windows.net/icons/organization/7ce64db3-50d7-4b6f-a57a-3e5cf2e2e4b5.png" alt="avatar"
+              class="rounded-circle img-fluid" style="width: 30px;">Organization</span>
+                </p>
+                <p class="mb-1" style="font-size: .77rem;">Association County Commissioners Of Georgia</p>
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Department: Other</p>
+                 <p class="mt-4 mb-1" style="font-size: .77rem;"><strong>Position: </strong>Director of Property & Liability Insurance Programs</p>
+               <!-- <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 <script>
 jQuery(document).ready(function(){
