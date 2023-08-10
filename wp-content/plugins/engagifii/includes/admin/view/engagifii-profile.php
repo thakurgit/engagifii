e021fa110400373e7ac0dc0d28a00f80117aac50
Guru0777 <gurpreetsingh@crescerance.com>
2023-08-10 10:22:33 +0530

    Template 2 changed

--- wp-content/plugins/engagifii/includes/admin/view/engagifii-profile.php
+++ wp-content/plugins/engagifii/includes/admin/view/engagifii-profile.php
@@ -1,7 +1,58 @@
 
   <!-- Bootstrap CSS CDN link -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" >
+<style>
+  body{margin-top:20px;
+background: #f5f5f5;
+}
+.theme-bg-white {
+    background-color: #fff !important;
+}
+.ui-w-100 {
+    width: 100px !important;
+    height: auto;
+}
+.ui-w-40 {
+    width: 40px !important;
+    height: auto;
+}
+.row-bordered>.col::before, .row-bordered>[class^="col-"]::before, .row-bordered>[class*=" col-"]::before, .row-bordered>[class^="col "]::before, .row-bordered>[class*=" col "]::before, .row-bordered>[class$=" col"]::before, .row-bordered>[class="col"]::before {
+    content: "";
+    position: absolute;
+    right: 0;
+    bottom: -1px;
+    left: 0;
+    display: block;
+    height: 0;
+    border-top: 1px solid rgba(24,28,33,0.06);
+}
+.row-bordered>.col::after, .row-bordered>[class^="col-"]::after, .row-bordered>[class*=" col-"]::after, .row-bordered>[class^="col "]::after, .row-bordered>[class*=" col "]::after, .row-bordered>[class$=" col"]::after, .row-bordered>[class="col"]::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: -1px;
+    display: block;
+    width: 0;
+    border-left: 1px solid rgba(24,28,33,0.06);
+}
 
+.ui-bg-cover {
+    background-color: rgba(0,0,0,0);
+    background-position: center center;
+    background-size: cover;
+}
+
+.ui-square {
+    padding-top: 100% !important;
+}
+.ui-square, .ui-rect, .ui-rect-30, .ui-rect-60, .ui-rect-67, .ui-rect-75 {
+    position: relative !important;
+    display: block !important;
+    padding-top: 100% !important;
+    width: 100% !important;
+}
+</style>
 <?php
  ?>
  <div class="pe-3 mb-3">
@@ -145,149 +196,199 @@ Atlanta, GA 30303</p>
         </div>	
     </div>
     </section>
-<section class="temp2 pe-3"  style="background-color: #eee; display:none">
-  <div class="container py-5">
-    <div class="row">
-      <div class="col">
-        <h3>My Profile</h3>
+<section class="temp2 pe-3"  style="background-color: #eee; display:none"><div class="layout-content">
+
+<!-- Content -->
+<div class="container flex-grow-1 container-p-y">
+
+  <!-- Header -->
+  <div class="container-m-nx container-m-ny theme-bg-white mb-4">
+    <div class="media col-md-10 col-lg-8 col-xl-7 py-5 mx-auto">
+      <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" class="d-block ui-w-100 rounded-circle">
+      <div class="media-body ml-5">
+        <h4 class="font-weight-bold mb-4">Nellie Maxwell</h4>
+
+        <div class="text-muted mb-4">
+          Lorem ipsum dolor sit amet, nibh suavitate qualisque ut nam. Ad harum primis electram duo, porro principes ei has.
+        </div>
+
+        <a href="javascript:void(0)" class="d-inline-block text-body">
+          <strong>234</strong>
+          <span class="text-muted">followers</span>
+        </a>
+        <a href="javascript:void(0)" class="d-inline-block text-body ml-3">
+          <strong>111</strong>
+          <span class="text-muted">following</span>
+        </a>
       </div>
     </div>
+    <hr class="m-0">
+  </div>
+  <!-- Header -->
 
-    <div class="row">
-      <div class="col-lg-4">
-        <div class="card mb-4">
-          <div class="card-body text-center">
-            <img src="http://www.accg.org/Staff%20Photos/Abercrombie_Ashley.jpg" alt="avatar"
-              class="rounded-circle img-fluid" style="width: 150px;">
-            <h5 class="my-3">Ms. Ashley Abercrombie</h5>
-            <p class="text-muted mb-1"><strong>PID:</strong> AACR7391</p>
-            <p class="text-muted mb-4">191 Peachtree Street, Suite 700 Atlanta, GA 30303</p>
-            <div class="d-flex justify-content-center mb-2">
-              <button type="button" class="btn btn-primary">Edit Profile</button>
-              
+  <div class="row">
+    <div class="col">
+
+      <!-- Info -->
+      <div class="card mb-4">
+        <div class="card-body">
+
+          <div class="row mb-2">
+            <div class="col-md-3 text-muted">Birthday:</div>
+            <div class="col-md-9">
+              May 3, 1995
             </div>
           </div>
+
+          <div class="row mb-2">
+            <div class="col-md-3 text-muted">Country:</div>
+            <div class="col-md-9">
+              <a href="javascript:void(0)" class="text-body">Canada</a>
+            </div>
+          </div>
+
+          <div class="row mb-2">
+            <div class="col-md-3 text-muted">Languages:</div>
+            <div class="col-md-9">
+              <a href="javascript:void(0)" class="text-body">English</a>
+            </div>
+          </div>
+
+          <h6 class="my-3">Contacts</h6>
+
+          <div class="row mb-2">
+            <div class="col-md-3 text-muted">Phone:</div>
+            <div class="col-md-9">
+              +0 (123) 456 7891
+            </div>
+          </div>
+
+          <h6 class="my-3">Interests</h6>
+
+          <div class="row mb-2">
+            <div class="col-md-3 text-muted">Favorite music:</div>
+            <div class="col-md-9">
+              <a href="javascript:void(0)" class="text-body">Rock</a>,
+              <a href="javascript:void(0)" class="text-body">Alternative</a>,
+              <a href="javascript:void(0)" class="text-body">Electro</a>,
+              <a href="javascript:void(0)" class="text-body">Drum &amp; Bass</a>,
+              <a href="javascript:void(0)" class="text-body">Dance</a>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-3 text-muted">Favorite movies:</div>
+            <div class="col-md-9">
+              <a href="javascript:void(0)" class="text-body">The Green Mile</a>,
+              <a href="javascript:void(0)" class="text-body">Pulp Fiction</a>,
+              <a href="javascript:void(0)" class="text-body">Back to the Future</a>,
+              <a href="javascript:void(0)" class="text-body">WALL·E</a>,
+              <a href="javascript:void(0)" class="text-body">Django Unchained</a>,
+              <a href="javascript:void(0)" class="text-body">The Truman Show</a>,
+              <a href="javascript:void(0)" class="text-body">Home Alone</a>,
+              <a href="javascript:void(0)" class="text-body">Seven Pounds</a>
+            </div>
+          </div>
+
         </div>
-        <div class="card mb-4 mb-lg-0">
-          <div class="card-body p-0">
-            <ul class="list-group list-group-flush rounded-3">
-              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
-                <i class="fas fa-globe fa-lg text-warning"></i>
-                <p class="mb-0"><strong>Tags: </strong>ACCG: All Staff, Assigned to Certified: Co, eLiability Beat, eRisk Connection, eSafety Theme, Mailings: County Line e-n</p>
-              </li>
-              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
-                <i class="fab fa-github fa-lg" style="color: #333333;"></i>
-                <p class="mb-0">Status: <span style="color:green;">Active</span></p>
-              </li>
-              <!-- <li class="list-group-item d-flex justify-content-left align-items-center p-3">
-                <i class="fab fa-twitter fa-lg" style="color: #55acee;"></i>
-                <p class="mb-0">@mdbootstrap</p>
-              </li>
-              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
-                <i class="fab fa-instagram fa-lg" style="color: #ac2bac;"></i>
-                <p class="mb-0">mdbootstrap</p>
-              </li>
-              <li class="list-group-item d-flex justify-content-left align-items-center p-3">
-                <i class="fab fa-facebook-f fa-lg" style="color: #3b5998;"></i>
-                <p class="mb-0">mdbootstrap</p>
-              </li> -->
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-8">
-        <div class="card mb-4">
-          <div class="card-body">
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Full Name</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">Ms. Ashley Abercrombie</p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Email</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">aabercrombie@accg.org</p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Phone</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">(404) 522-5022</p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Mobile</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">(404) 522-5022</p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Address</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">191 Peachtree Street, Suite 700
-Atlanta, GA 30303</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <div class="card mb-4 mb-md-0">
-              <div class="card-body">
-              <p class="mb-4"><span class="text-primary font-italic me-1"><img src="https://engagifiistorage.blob.core.windows.net/icons/organization/7ce64db3-50d7-4b6f-a57a-3e5cf2e2e4b5.png" alt="avatar"
-              class="rounded-circle img-fluid" style="width: 30px;">Organization</span> 
-                </p>
-                <p class="mb-1" style="font-size: .77rem;">Association County Commissioners Of Georgia</p>
-                
-                <p class="mt-4 mb-1" style="font-size: .77rem;">Department: Other</p>
-                
-                <!-- <p class="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
-                
-                <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
-                
-                <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p> -->
-                <!-- https://engagifiistorage.blob.core.windows.net/icons/organization/7ce64db3-50d7-4b6f-a57a-3e5cf2e2e4b5.png -->
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="card mb-4 mb-md-0">
-            <div class="card-body">
-                <p class="mb-4"><span class="text-primary font-italic me-1"><img src="https://engagifiistorage.blob.core.windows.net/icons/organization/7ce64db3-50d7-4b6f-a57a-3e5cf2e2e4b5.png" alt="avatar"
-              class="rounded-circle img-fluid" style="width: 30px;">Organization</span> 
-                </p>
-                <p class="mb-1" style="font-size: .77rem;">Association County Commissioners Of Georgia</p>
-                
-                <p class="mt-4 mb-1" style="font-size: .77rem;">Department: Other</p>
-                
-                 <p class="mt-4 mb-1" style="font-size: .77rem;"><strong>Position: </strong>Director of Property & Liability Insurance Programs</p>
-                
-               <!-- <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
-                
-                <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p> -->
-               
-              </div>
-            </div>
+        <div class="card-footer text-center p-0">
+          <div class="row no-gutters row-bordered row-border-light">
+            <a href="javascript:void(0)" class="d-flex col flex-column text-body py-3">
+              <div class="font-weight-bold">24</div>
+              <div class="text-muted small">posts</div>
+            </a>
+            <a href="javascript:void(0)" class="d-flex col flex-column text-body py-3">
+              <div class="font-weight-bold">51</div>
+              <div class="text-muted small">videos</div>
+            </a>
+            <a href="javascript:void(0)" class="d-flex col flex-column text-body py-3">
+              <div class="font-weight-bold">215</div>
+              <div class="text-muted small">photos</div>
+            </a>
           </div>
         </div>
       </div>
+      <!-- / Info -->
+
+      
+    </div>
+    <div class="col-xl-4">
+
+      <!-- Side info -->
+      <div class="card mb-4">
+        <div class="card-body">
+          <a href="javascript:void(0)" class="btn btn-primary rounded-pill">+&nbsp; Follow</a>
+          &nbsp;
+          <a href="javascript:void(0)" class="btn icon-btn btn-default md-btn-flat rounded-pill">
+            <span class="ion ion-md-mail"></span>
+          </a>
+        </div>
+        <hr class="border-light m-0">
+        <div class="card-body">
+          <p class="mb-2">
+            <i class="ion ion-md-desktop ui-w-30 text-center text-lighter"></i> UI/UX Designer</p>
+          <p class="mb-2">
+            <i class="ion ion-ios-navigate ui-w-30 text-center text-lighter"></i> London, United Kingdom</p>
+          <p class="mb-0">
+            <i class="ion ion-md-globe ui-w-30 text-center text-lighter"></i>
+            <a href="javascript:void(0)" class="text-body">website.com</a>
+          </p>
+        </div>
+        <hr class="border-light m-0">
+        <div class="card-body">
+          <a href="javascript:void(0)" class="d-block text-body mb-2">
+            <i class="ion ion-logo-twitter ui-w-30 text-center text-twitter"></i> @nmaxwell
+          </a>
+          <a href="javascript:void(0)" class="d-block text-body mb-2">
+            <i class="ion ion-logo-facebook ui-w-30 text-center text-facebook"></i> nmaxwell
+          </a>
+          <a href="javascript:void(0)" class="d-block text-body mb-0">
+            <i class="ion ion-logo-instagram ui-w-30 text-center text-instagram"></i> nmaxwell
+          </a>
+        </div>
+      </div>
+      <!-- / Side info -->
+
+      <!-- Skills -->
+      <div class="card mb-4">
+        <div class="card-header">Skills</div>
+        <div class="card-body">
+
+          <div class="mb-1">HTML - <small class="text-muted">80%</small></div>
+          <div class="progress mb-3" style="height: 4px;">
+            <div class="progress-bar bg-secondary" style="width: 80%;"></div>
+          </div>
+
+          <div class="mb-1">CSS - <small class="text-muted">95%</small></div>
+          <div class="progress mb-3" style="height: 4px;">
+            <div class="progress-bar bg-success" style="width: 95%;"></div>
+          </div>
+
+          <div class="mb-1">Javascript - <small class="text-muted">90%</small></div>
+          <div class="progress mb-3" style="height: 4px;">
+            <div class="progress-bar bg-warning" style="width: 90%;"></div>
+          </div>
+
+          <div class="mb-1">UI/UX - <small class="text-muted">80%</small></div>
+          <div class="progress" style="height: 4px;">
+            <div class="progress-bar bg-danger" style="width: 80%;"></div>
+          </div>
+
+        </div>
+        <a href="javascript:void(0)" class="card-footer d-block text-center text-body small font-weight-semibold">SHOW ALL SKILLS</a>
+      </div>
+      <!-- / Skills -->
+
+      
     </div>
   </div>
+
+</div>
+<!-- / Content -->
+
+
+
+</div>
 </section>
 <script>
 jQuery(document).ready(function(){
