d4d8e0d0e9eaf2f662eeedbbd8caa79a8b034cf7
thakurgit <prakashthakur@crescerance.com>
2023-10-09 17:24:33 +0530

    public officials

--- wp-content/plugins/engagifii/includes/public/views/public_officials.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials.php
@@ -1,2 +1,21 @@
-<?php
-echo 'abc';
+<style type="text/css">
+  
+ .session-tab button {
+	border-bottom:3px solid transparent !important;
+	color:#333 !important;
+ }
+  .session-tab button.active{
+	border-bottom-color:#002474  !important;
+ }
+</style>
+     <ul class="nav nav-pills mb-3 justify-content-center session-tab border-bottom" id="pills-tab" role="tablist">
+         				
+                 <li class="nav-item mx-2" role="presentation">
+    <button class="nav-link bg-transparent border-0 rounded-0 active" id="" data-toggle="pill" data-target="#tab-1" type="button" role="tab" aria-controls="home" aria-selected="true">State Senate(12)</button>
+  </li>
+  <li class="nav-item mx-2" role="presentation">
+    <button class="nav-link bg-transparent border-0 rounded-0" id="" data-toggle="pill" data-target="#tab-2" type="button" role="tab" aria-controls="home" aria-selected="true">State House(7)</button>
+  </li>
+       
+     
+      </ul>
