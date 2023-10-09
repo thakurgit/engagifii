effcfdb3a15ce4004b0a1a2c5f6b13e091e7880e
thakurgit <prakashthakur@crescerance.com>
2023-10-09 17:27:53 +0530

    public officials

--- wp-content/plugins/engagifii/includes/public/views/public_officials.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials.php
@@ -16,6 +16,18 @@
   <li class="nav-item mx-2" role="presentation">
     <button class="nav-link bg-transparent border-0 rounded-0" id="" data-toggle="pill" data-target="#tab-2" type="button" role="tab" aria-controls="home" aria-selected="true">State House(7)</button>
   </li>
+  <li class="nav-item mx-2" role="presentation">
+    <button class="nav-link bg-transparent border-0 rounded-0" id="" data-toggle="pill" data-target="#tab-2" type="button" role="tab" aria-controls="home" aria-selected="true">State Senate Committees(5)</button>
+  </li>
+  <li class="nav-item mx-2" role="presentation">
+    <button class="nav-link bg-transparent border-0 rounded-0" id="" data-toggle="pill" data-target="#tab-2" type="button" role="tab" aria-controls="home" aria-selected="true">State House Committees(6)</button>
+  </li>
+   <li class="nav-item mx-2" role="presentation">
+    <button class="nav-link bg-transparent border-0 rounded-0" id="" data-toggle="pill" data-target="#tab-2" type="button" role="tab" aria-controls="home" aria-selected="true">County Delegations(4)</button>
+  </li>
+  <li class="nav-item mx-2" role="presentation">
+    <button class="nav-link bg-transparent border-0 rounded-0" id="" data-toggle="pill" data-target="#tab-2" type="button" role="tab" aria-controls="home" aria-selected="true">Congressional Delegations(11)</button>
+  </li>
        
      
       </ul>
