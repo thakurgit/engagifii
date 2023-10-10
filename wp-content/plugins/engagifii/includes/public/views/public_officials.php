041cd6eb5b2eca035dab9b14997287e61837bb79
thakurgit <prakashthakur@crescerance.com>
2023-10-10 12:15:56 +0530

    public officials

--- wp-content/plugins/engagifii/includes/public/views/public_officials.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials.php
@@ -3,9 +3,7 @@
 	$pJSON = file_get_contents($peopleurl);
 	 $peopleDATA   = json_decode($pJSON);
 	//print_r($peopleDATA);
-	foreach ($peopleDATA as $key => $value) {
-     print_r($key);
- }
+	
 ?>
 <style type="text/css">
   
@@ -18,15 +16,13 @@
  }
 </style>
      <ul class="nav nav-pills mb-3 justify-content-center session-tab border-bottom" id="pills-tab" role="tablist">
-      <?php $i=1;
+      <?php $i=1; $class='';
 	  	foreach ($peopleDATA as $key => $value) {
-			$class='';
 			if($i==1){
 				$class =' active';
 			}
     echo '<li class="nav-item mx-2" role="presentation">
-    <button class="nav-link bg-transparent border-0 rounded-0'.$class.'" id="" data-toggle="pill" data-target="#tab-'.$i.'" type="button" role="tab" aria-controls="home" aria-selected="true">'.$key.'</button>
-  </li>';
+    <button class="nav-link bg-transparent border-0 rounded-0'.$class.'" id="" data-toggle="pill" data-target="#tab-'.$i.'" type="button" role="tab" aria-controls="home" aria-selected="true">'.$key.'('.count($value).')</button></li>';
  $i++; }
 	  ?>   		
       </ul>
