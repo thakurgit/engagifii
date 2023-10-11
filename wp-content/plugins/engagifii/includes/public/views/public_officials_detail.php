6cf320647aff1b44122c8ee4c96202970685396f
thakurgit <prakashthakur@crescerance.com>
2023-10-11 15:36:50 +0530

    public officials detail

--- wp-content/plugins/engagifii/includes/public/views/public_officials_detail.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials_detail.php
@@ -15,11 +15,11 @@ $apiurl = 'https://builtin-crm.azurewebsites.net/api/v1/Advocacy/elected/officia
         <div>
         	<h4><?php echo $response->firstName.' '.$response->middleName.' '.$response->lastName; ?> (<?php echo $response->officialNameLabel; ?>)</h4>
             <div class="row">
-            	<div class="col-3"><strong>District: </strong><?php echo $response->districtCode; ?></div>
-            	<div class="col-3"><strong>City of Residence: </strong><?php echo $response->residence; ?></div>
-            	<div class="col-3"><strong>Role: </strong><?php echo $response->role; ?></div>
-            	<div class="col-3"><strong>Party: </strong><?php echo $response->party; ?></div>
-            	<div class="col-3"><strong>County: </strong><?php echo $response->counties[0]->countyName; 
+            	<div class="col-auto pr-lg-5"><strong>District: </strong><?php echo $response->districtCode; ?></div>
+            	<div class="col-auto pr-lg-5"><strong>City of Residence: </strong><?php echo $response->residence; ?></div>
+            	<div class="col-auto pr-lg-5"><strong>Role: </strong><?php echo $response->role; ?></div>
+            	<div class="col-auto pr-lg-5"><strong>Party: </strong><?php echo $response->party; ?></div>
+            	<div class="col-auto pr-lg-5"><strong>County: </strong><?php echo $response->counties[0]->countyName; 
 					if(count($response->counties) > 1){
 						echo ' <span class="dropdown dropright"><a type="button" data-toggle="dropdown">+'.count($response->counties) .' more</a><div class="dropdown-menu py-0"><ul class="list-group list-group-flush">';	
 						foreach ($response->counties as $key => $value) {
