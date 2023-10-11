a3f8eb201d71703c3510866edee32e69f21ffc79
thakurgit <prakashthakur@crescerance.com>
2023-10-11 13:18:23 +0530

    public officials detail

--- wp-content/plugins/engagifii/includes/public/views/public_officials_detail.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials_detail.php
@@ -2,8 +2,18 @@
 $apiurl = 'https://builtin-crm.azurewebsites.net/api/v1/Advocacy/elected/officials-all-tabs-list/people';
  $peopleurl = 'https://engagifiwebstg.wpengine.com/gsba/wp-content/plugins/wp-front-end-profile/views/official_detail.txt';
 	$pJSON = file_get_contents($peopleurl);
-	 $peopleDATA   = json_decode($pJSON);
-	//print_r($peopleDATA);
-	echo 'abc';
+	 $response   = json_decode($pJSON);
+	print_r($response);
+	
 	
 ?>
+<div class="container-fluid bg-light border">
+	<div class="d-flex">
+    	<div class="rounded-circle overflow-hidden mr-4" style="width:100px; height:100px">
+        	<img src="<?php echo $response->profilePic; ?>" alt="" class="img-fluid">
+        </div>
+        <div>
+        	<h3><?php echo $response->firstName.' '.$response->middleName.' '.$response->lastName; ?></h3>
+        </div>
+    </div>
+</div>
