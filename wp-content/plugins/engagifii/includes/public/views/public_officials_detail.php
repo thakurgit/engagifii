6fb1effb4698829e98e321d5e2637560a4e72397
thakurgit <prakashthakur@crescerance.com>
2023-10-11 13:41:05 +0530

    public officials detail

--- wp-content/plugins/engagifii/includes/public/views/public_officials_detail.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials_detail.php
@@ -19,8 +19,11 @@ $apiurl = 'https://builtin-crm.azurewebsites.net/api/v1/Advocacy/elected/officia
             	<div class="col-3"><strong>City of Residence: </strong><?php echo $response->residence; ?></div>
             	<div class="col-3"><strong>Role: </strong><?php echo $response->role; ?></div>
             	<div class="col-3"><strong>Party: </strong><?php echo $response->party; ?></div>
-            	<div class="col-3"><strong>County: </strong><?php echo $response->counties[0]->countyName; ?>
-                	
+            	<div class="col-3"><strong>County: </strong><?php echo $response->counties[0]->countyName; 
+					if(count($response->counties) > 1){
+						echo '+'.count($response->counties)-1 .' more';	
+					}
+				 ?>
                 </div>
             </div>
         </div>
