d19c9f8f3104608409a7ae8a7f381228bb489cc4
thakurgit <prakashthakur@crescerance.com>
2023-10-12 13:34:31 +0530

    public officials detail

--- wp-content/plugins/engagifii/includes/public/views/public_officials.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials.php
@@ -54,7 +54,7 @@ $apiurl = 'https://builtin-crm.azurewebsites.net/api/v1/Advocacy/elected/officia
 				$class =' active';
 			}
     echo '<li class="nav-item mx-2" role="presentation">
-    <button class="nav-link bg-transparent border-0 rounded-0'.$class.'" id="" data-toggle="pill" data-target="#tab-'.$i.'" type="button" role="tab" aria-controls="home" aria-selected="true">'.$name.' <b>('.count($value).')</b></button></li>';
+    <button class="nav-link bg-transparent border-0 rounded-0 px-0'.$class.'" id="" data-toggle="pill" data-target="#tab-'.$i.'" type="button" role="tab" aria-controls="home" aria-selected="true">'.$name.' <b>('.count($value).')</b></button></li>';
  $i++; }
 	  ?>   		
       </ul>
