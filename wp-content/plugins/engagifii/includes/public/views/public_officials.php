9a74c53a7843b2ee616d8549903f70ac870db3dd
thakurgit <prakashthakur@crescerance.com>
2023-10-12 13:57:49 +0530

    public officials detail

--- wp-content/plugins/engagifii/includes/public/views/public_officials.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials.php
@@ -19,7 +19,7 @@ $apiurl = 'https://builtin-crm.azurewebsites.net/api/v1/Advocacy/elected/officia
 <div class="container-fluid">
 <div class="row">
 	<div class="col-md-4">
-      <input type="text" placeholder="search Public Official..." class="form-control">
+      <input type="text" placeholder="Search Public Official..." class="form-control">
     </div>
 </div>
 </div>
@@ -53,7 +53,7 @@ $apiurl = 'https://builtin-crm.azurewebsites.net/api/v1/Advocacy/elected/officia
 			if($i==1){
 				$class =' active';
 			}
-    echo '<li class="nav-item mx-2" role="presentation">
+    echo '<li class="nav-item mr-3" role="presentation">
     <button class="nav-link bg-transparent border-0 rounded-0 px-0'.$class.'" id="" data-toggle="pill" data-target="#tab-'.$i.'" type="button" role="tab" aria-controls="home" aria-selected="true">'.$name.' <b>('.count($value).')</b></button></li>';
  $i++; }
 	  ?>   		
