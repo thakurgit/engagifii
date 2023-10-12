08a03740149e26cb34ca5f01de034b36e9522636
thakurgit <prakashthakur@crescerance.com>
2023-10-12 17:14:29 +0530

    public officials api

--- wp-content/plugins/engagifii/includes/public/views/public_officials.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials.php
@@ -10,9 +10,7 @@ $queryString = http_build_query($queryParameters);
 if (!empty($queryString)) {
     $url .= '?' . $queryString;
 }
-$payload='{
-  "searchText": "string"
-}';
+$payload='';
 curl_setopt_array($curl, array(  
   CURLOPT_URL => $url,
   CURLOPT_RETURNTRANSFER => true,
