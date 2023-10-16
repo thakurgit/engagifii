bb368d5ded772f0dad6446241261a0d28ad858f3
prakash thakur <prakashthakur@crescerance.com>
2023-10-16 14:51:33 +0530

    public offcials api

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -1363,9 +1363,9 @@ wp_die();
 	
 	    public function publicOfficalsearchData(){
         $postedData = $this->_publicOfficialSearch();
-        $dataResponse = $this->submitApiRequest("legislative/public-bills/elected/officials-all-tabs-list", json_decode($postedData), "POST", 'legislation');
+        $dataResponse = $this->submitApiRequest("legislative/public-bills/elected/officials-all-tabs-list", $postedData, "POST", 'legislation');
         header("Content-Type: application/json");   
-        //echo json_encode($dataResponse);
+        echo json_encode($dataResponse);
 		print_r(json_encode($postedData));
 		die;
         wp_die();
