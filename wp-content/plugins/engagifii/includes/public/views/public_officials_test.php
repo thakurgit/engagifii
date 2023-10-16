832b25c8b042d479c23d6e87db0dea9fd9e3dd47
thakurgit <prakashthakur@crescerance.com>
2023-10-16 20:37:46 +0530

    public offocil search

--- wp-content/plugins/engagifii/includes/public/views/public_officials_test.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials_test.php
@@ -240,7 +240,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           },
           success: function(response) { 
 		  	const data =   response.api_response; 
-			const data = JSON.parse(JSON.stringify(data))
+			data = JSON.parse(JSON.stringify(data))
 			let populationArr = Object.entries(data); 
 			console.log(data); 
 			console.log(populationArr); 
