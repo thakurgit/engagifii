d79778f8d34251f33e6719b9fe1fc97974014bf0
prakash thakur <prakashthakur@crescerance.com>
2023-10-16 13:13:19 +0530

    public offcials api

--- wp-content/plugins/engagifii/includes/public/views/public_officials_test.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials_test.php
@@ -1,7 +1,7 @@
 <?php
     $obj      =  new Engagifii_API();
     $publicOfficial = $obj->publicOfficial();
-	print_r(json_encode($publicOfficial));
+	$publicOfficial = json_encode($publicOfficial);
 
 $options = get_option('ebt_api_settings');
 $tenant_code          = $options['lbt_tenant_code']['tenant_code'];
