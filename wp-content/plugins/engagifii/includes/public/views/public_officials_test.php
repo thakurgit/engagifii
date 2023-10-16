83bfd46302031876c2963934ab9c082d13f46565
thakurgit <prakashthakur@crescerance.com>
2023-10-16 19:11:22 +0530

    public offocil search

--- wp-content/plugins/engagifii/includes/public/views/public_officials_test.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials_test.php
@@ -244,7 +244,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			var count1=[];
 			data.forEach(function(value, index) {
 			  console.log(value);
-			}
+			});
 			console.log(count1);
 			jQuery('#pills-tab li').each(function(){
 				jQuery(this).find('button b').text('('+count1[jQuery(this).index()]+')');	
