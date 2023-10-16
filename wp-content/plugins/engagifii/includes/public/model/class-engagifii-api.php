592ec3f3293f54cd62985167ace023c33c7c2564
prakash thakur <prakashthakur@crescerance.com>
2023-10-16 12:35:30 +0530

    public offcials api

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-api.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-api.php
@@ -644,8 +644,8 @@ public function _popOverSpeakerData3($id, $instructorData){
 		$responseArray = array();
 		$apiUrl = 'legislative/public-bills/elected/officials-all-tabs-list';
 		$response =  $this->submitApiRequest($apiUrl, $postData, 'POST', 'legislation');
-		$responseArray = json_decode($response['api_response'], true);
-		return $responseArray;
+		//$responseArray = json_decode($response['api_response'], true);
+		return $response;
 		
 	}
 
