c9f788d8f405188ff566eaa140fb4da65e2ab115
prakash thakur <prakashthakur@crescerance.com>
2023-10-19 01:02:22 +0530

    public official datattable

--- wp-content/plugins/engagifii/includes/public/views/public_officials_test.php
+++ wp-content/plugins/engagifii/includes/public/views/public_officials_test.php
@@ -76,7 +76,7 @@ foreach ($seqColumns as $key => $value) {
 			$list = $value;
 			?>
 <div class="tab-pane fade <?php echo $class; ?>" id="tab-<?php echo $k; ?>" role="tabpanel">
-	<table id="ebtmaintable"  data-tab="<?php echo $key;?>"  class="tabData table table-bordered border-0 table-striped" style="width: 100% !important;">
+	<table id="ebtmaintable"  class=" tabData table table-bordered border-0 table-striped" style="width: 100% !important;">
       <thead> 
         <tr> 
         	<?php echo $tableHeader; ?>        
@@ -153,20 +153,11 @@ if($committeesList){
 </div>
       <?php $k++; } ?>
        </div>
-<div class="container-fluid mb-4">
- 
- 
-</div>
-	
        <script>
   var titleColumn = '<?php echo $title_key; ?>';
   
-  $('table').each(function(){
-  var tabname='';
-	 	tabname = $(this).attr('data-tab'); 
-  });
- var table = new DataTable('table.tabData', {
-			  "pageLength": '10',
+ var table = $('table.tabData').DataTable({
+			  "pageLength": '5',
 			  "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
 			  "dom": '<"row no-gutters"<"col-12 custom-scroll border-left border-right border-bottom"t">><"row"<"col-sm-5 pt-3"l><"col-sm-7 pt-3"p">>',
 			  "bInfo":false,
@@ -226,7 +217,8 @@ if($committeesList){
 <?php
   if($title_key > -1){
 ?>
-  $('#ebtmaintable thead tr th:eq('+titleColumn+')').each( function (i) { 
+  $('table').each( function (i) { 
+  $(this).find('thead tr th:eq('+titleColumn+')').each( function (i) { 
          var title = $(this).text();
         $(this).html( '<div class="position-relative input-group search-dt"><input type="text" id="searchclass" placeholder="Search Public official.." class="form-control form-control-sm pr-4 shadow-none" value=""/><div class="input-group-append"><span class="input-group-text px-1 bg-white rounded-right" ><i class="fal fa-search"></i></span></div><button type="button" class="clear-search btn position-absolute p-1 px-2 shadow-none" style="right:21px; top:-1px; z-index:3;display:none"><i class="fal fa-times"></i></button></div>' );
 
@@ -242,28 +234,29 @@ function delay(callback, ms) {
 }
   $( 'input', this ).keyup(delay(function (e) {
 	  var titlesearch = this.value;
-            if ( table.column(titleColumn).search() !== titlesearch ) {
-				table.column(titleColumn).search(titlesearch).draw();
+           if ( table.columns(titleColumn).search() !== titlesearch ) {
+				table.columns(titleColumn).search(titlesearch).draw();
             }
 }, 500));
 
 
  $( 'input', this ).keyup(function(e){
 	if(this.value.length!=0){
-				$('.clear-search').show();
+				$(this).parents('th').find('.clear-search').show();
 			} else {
-				$('.clear-search').hide();
+				$(this).parents('th').find('.clear-search').hide();
 			} 
  });
 $('th .clear-search').click(function(e){
-	 $('#searchclass').val('');
-	$('.clear-search').hide();
+	 $(this).parents('th').find('#searchclass').val('');
+	$(this).parents('th').find('.clear-search').hide();
 	e.stopPropagation();
 	table.column(titleColumn).search('').draw();
  });
 
     } );
 	
+});
 	$(document).ready(function (){    
     $('#searchclass, .search-dt span').on('click', function(e){
        e.stopPropagation();    
