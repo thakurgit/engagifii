b665356d6a47bacdfa779ea0d0fe1262a9b598b6
Guru0777 <gurpreetsingh@crescerance.com>
2023-11-20 12:35:59 +0530

    report url change to qa

--- wp-content/plugins/engagifii/includes/public/views/shortcode/legislative-reports.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/legislative-reports.php
@@ -90,7 +90,7 @@ foreach ($reportsResponses as $value) {
 				$name = $values['billReportName'];
 				$id = $values['billReportId'];
 				?>
-				<a href="https://engagifii.engagifii-preview6.com/public/lbt-report/<?php echo $id; ?>/schedule-false" style ="color: #002473;" target="_blank">
+				<a href="https://engagifii.engagifii-qa.com/public/lbt-report/<?php echo $id; ?>/schedule-false" style ="color: #002473;" target="_blank">
 				<div class="row mb-2">
 				       <div class="col-10">
                             <?php echo $name; ?>
