335f22b53e6fb1084d7e5ca7e0b043634e4ca7ec
Guru0777 <gurpreetsingh@crescerance.com>
2023-11-21 16:10:53 +0530

    report url changed to production

--- wp-content/plugins/engagifii/includes/public/views/shortcode/legislative-reports.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/legislative-reports.php
@@ -102,7 +102,7 @@ foreach ($reportsResponses as $value) {
 				$name = $values['billReportName'];
 				$id = $values['billReportId'];
 				?>
-				<a href="https://<?php echo $tenant; ?>.engagifii-qa.com/public/lbt-report/<?php echo $id; ?>/schedule-false" style ="color: #002473;" target="_blank">
+				<a href="https://<?php echo $tenant; ?>.engagifii.com/public/lbt-report/<?php echo $id; ?>/schedule-false" style ="color: #002473;" target="_blank">
 				<div class="row mb-2">
 				       <div class="col-10">
                             <?php echo $name; ?>
