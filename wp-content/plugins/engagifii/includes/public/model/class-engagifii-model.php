94b10299ed5b753fb012a20c12389e870e3e371f
prakash thakur <prakashthakur@crescerance.com>
2024-01-15 15:31:08 +0530

    payload

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -1357,8 +1357,7 @@ wp_die();
     public function countLegislationFilterData()
     {
         $postedData = $this->_prepareLegislationPostCountData();
-        print_r(json_encode($postedData));
-		die;
+        //print_r(json_encode($postedData));
         $dataResponse = $this->submitApiRequest("legislative/public-bills/all-filter-list/count", $postedData, "POST", 'legislation');
         header("Content-Type: application/json");     
         echo json_encode($dataResponse);
