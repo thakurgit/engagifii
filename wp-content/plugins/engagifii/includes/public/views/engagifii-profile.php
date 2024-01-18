3e1fbc9454902ba6652cd0244a52b232f35ad86b
prakash thakur <prakashthakur@crescerance.com>
2024-01-18 14:04:32 +0530

    psba dashboard

--- wp-content/plugins/engagifii/includes/public/views/engagifii-profile.php
+++ wp-content/plugins/engagifii/includes/public/views/engagifii-profile.php
@@ -164,7 +164,19 @@ echo "<br><br><h5 class='text-center'>A person with this Email ID doesn't exist.
   </div>
   </div>
   <!-- Header -->
-<div class="container-fluid">
+  <div class="container-fluid">
+  <div class="accordion" id="accordionExample">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Profile
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
   <div class="row">
     <div class="col-8">
 
@@ -309,10 +321,26 @@ echo "<br><br><h5 class='text-center'>A person with this Email ID doesn't exist.
 
 </div>
 <!-- / Content -->
-
-
-
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h2 class="mb-0">
+        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Events and Training
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+        
+      </div>
+    </div>
+  </div>
 </div>
+</div>
+
 </div>
 </div>
 <?php } ?>
