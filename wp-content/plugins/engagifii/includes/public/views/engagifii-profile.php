8b34993a58ecdc2c27d18ba61a40e9c808cb8843
prakash thakur <prakashthakur@crescerance.com>
2024-01-18 18:41:57 +0530

    psba dashboard

--- wp-content/plugins/engagifii/includes/public/views/engagifii-profile.php
+++ wp-content/plugins/engagifii/includes/public/views/engagifii-profile.php
@@ -64,9 +64,9 @@ if (! is_user_logged_in()) {
 ?>
  <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" >
   <link href="https://site-assets.fontawesome.com/releases/v6.2.1/css/all.css" rel="stylesheet" >-->
-  <div class="d-flex justify-content-end">
+  <div class="d-flex justify-content-end px-3 mb-3">
     
-    <a href="<?php echo esc_url(wp_logout_url('')); ?>"><?php esc_html_e('Logout', 'wpfep'); ?></a>
+    <a class="btn-secondary btn-sm btn-xs" href="<?php echo esc_url(wp_logout_url('')); ?>"><?php esc_html_e('Logout', 'wpfep'); ?></a>
     
 </div>
 <?php 
