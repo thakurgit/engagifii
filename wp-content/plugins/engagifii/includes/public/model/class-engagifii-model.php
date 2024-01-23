58b3f0ceb749937c9d3d10484759485fca62495f
prakash thakur <prakashthakur@crescerance.com>
2024-01-23 13:23:43 +0530

    credit earned dashboard

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -1767,7 +1767,7 @@ wp_die();
             ## row data
             $nestedData['coursename'] = '<a class="d-flex align-items-center" href="'.site_url().'/course-details/?courseId='.$value->id.'"><img src="'.$value->courseIcon.'" class="img-fluid mr-3 img-icon-lg" alt="course-icon">'.$value->courseName.'</a>';
             $nestedData['coursetype'] = $value->objectType;
-            $nestedData['completiondate '] = '';
+            $nestedData['completiondate'] = '';
             $nestedData['creditHours'] = $value->creditHours;
             $courseTag = $value->courseTags;
             $allTags = array();
