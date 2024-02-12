834d186b81282a9b5b83a5d94e5bddfd782ea546
prakash thakur <prakashthakur@crescerance.com>
2024-02-12 13:00:21 +0530

    event detail page go back

--- wp-content/plugins/engagifii/includes/public/views/events/grid-listing-details.php
+++ wp-content/plugins/engagifii/includes/public/views/events/grid-listing-details.php
@@ -58,9 +58,9 @@
 <?php
 $current_user = wp_get_current_user();
 if ( in_array( 'subscriber', $current_user->roles ) ) {?> 
-    <a href="<?php echo $events_page;?>" class="go-back"><i class="fal fa-arrow-left mr-2"></i> Go Back </a>
-<?php } else { ?>
     <a href="<?php echo site_url().'/engagifii-profile/events/';?>" class="go-back"><i class="fal fa-arrow-left mr-2"></i> Go Back </a>
+<?php } else { ?>
+    <a href="<?php echo $events_page;?>" class="go-back"><i class="fal fa-arrow-left mr-2"></i> Go Back </a>
 <?php } 
 ?>
 
