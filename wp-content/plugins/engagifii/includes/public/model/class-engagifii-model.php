65fecead4ca4ba814dc2e7229d94727c9a561b64
prakash thakur <prakashthakur@crescerance.com>
2024-02-16 16:52:12 +0530

    portal tag

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -2625,10 +2625,6 @@ wp_die();
 
         $data = array();
         foreach ($collection as $key => $row) {
-			$portalTag = $row->tags;
-			if (!in_array("portal", $portalTag)){
-				continue;
-			}
            
             /* getdata for tables */
             $nestedData = array();
