489a3290595b9518e93a0b4a0ac60a6e0f6d25da
prakash thakur <prakashthakur@crescerance.com>
2024-02-16 16:22:54 +0530

    register button tooltip

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -2721,7 +2721,10 @@ wp_die();
                 $default_RegisterBtn .= '<span class="d-inline-block" tabindex="0" data-toggle="tooltip" data-placement="right" title="'.$tooltip.'"><button type="button" id="onlocation" class="btn btn-primary  px-3 py-1"  disabled style="pointer-events: none;">Register</button></span>';
             }
             else{
-                $default_RegisterBtn .= '<a href="https://psba.engagifii-preview4.com/auth-callback/pages/home#access_token='.$_SESSION['accesstoken'].'&source=external&tpath=https://psba.engagifii-preview4.com/pages/events/'. $default_Id .'/'.$row->registrationWorkflows[0]->registrationWorkflowId.'/'.$row->registrationWorkflows[0]->roleId.'/eventregpub/signup/overview" target="_blank" class="btn btn-primary px-3 py-1" >Register</a>';
+				if($row->eventRegistrationState=='RegistrationOn'){
+             	   $tooltip = 'Registration opens from '.date('M d, Y', strtotime($row->registrationStartFrom));
+				}
+                $default_RegisterBtn .= '<span class="d-inline-block" tabindex="0" data-toggle="tooltip" data-placement="right" title="'.$tooltip.'"><a href="https://psba.engagifii-preview4.com/auth-callback/pages/home#access_token='.$_SESSION['accesstoken'].'&source=external&tpath=https://psba.engagifii-preview4.com/pages/events/'. $default_Id .'/'.$row->registrationWorkflows[0]->registrationWorkflowId.'/'.$row->registrationWorkflows[0]->roleId.'/eventregpub/signup/overview" target="_blank" class="btn btn-primary px-3 py-1" >Register</a></span>';
             }
             
            
