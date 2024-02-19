1bf6d9ba9d063f9b22a7ece92136e65dfc817fff
prakash thakur <prakashthakur@crescerance.com>
2024-02-19 16:35:00 +0530

    profile settings

--- wp-content/plugins/engagifii/includes/admin/view/dashboard-settings.php
+++ wp-content/plugins/engagifii/includes/admin/view/dashboard-settings.php
@@ -1,13 +1,12 @@
+<div class="wrap dashoboard-settings <?php if($tab == 'dashboard-settings'){ echo 'show';}else {echo 'hide'; }?>" >
+<div class="engagifii-setting m-tlr-20">
 <?php 	$options = get_option( 'ebt_api_settings' );
 	$tenant_url= $options['ebt_tenant_code']['engagifii_url'];
  if($tenant_url!='psba'){
-//echo '<h4>Profile settings is not allowed</h4>';
-return; } ?>
-<div class="wrap dashoboard-settings <?php if($tab == 'dashboard-settings'){ echo 'show';}else {echo 'hide'; }?>" >
-<div class="engagifii-setting m-tlr-20">
-<?php 
-$options = get_option( 'ebt_api_settings' );
+echo '<h4>Profile settings is not allowed</h4>';
+} else { 
 ?>
-<h2>abc</h2>
+<h2>Profile settings is in progress..</h2>
+<?php } ?>
 </div>
 </div>
