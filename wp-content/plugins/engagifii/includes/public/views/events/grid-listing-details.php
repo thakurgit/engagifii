01815a9b7f12f50f18684c4dc1c6585d4d7f4bee
prakash thakur <prakashthakur@crescerance.com>
2024-02-20 21:52:53 +0530

    button tooltip event

--- wp-content/plugins/engagifii/includes/public/views/events/grid-listing-details.php
+++ wp-content/plugins/engagifii/includes/public/views/events/grid-listing-details.php
@@ -145,7 +145,7 @@ if ( strpos($url,'engagifii-profile') !== false ) {?>
 		   } else if($isAlreadyRegistered){
                 $alreadyRegisteredText = "Already Registered";
                 $tooltip = preg_replace('/(?<!\ )[A-Z]/', ' $0', $alreadyRegisteredText); ?>
-                <div class="mt-auto"><span class="d-inline-block" tabindex="0" data-toggle="tooltip" data-placement="right" title="<?php echo $tooltip;?>"><button type="button" id="onlocation" class="btn btn-primary  px-3 py-1"  disabled style="pointer-events: none;">Register</button></span></div>
+                <div class="mt-auto"><span class="d-inline-block" tabindex="0" data-toggle="tooltip" data-placement="right" title="<?php echo $tooltip;?>"><button type="button"  class="btn btn-primary  px-3 py-1"  disabled style="pointer-events: none;">Register</button></span></div>
 		 <?php } else{
 			  //$tooltip = 'Registration opens from '.date('M d, Y', strtotime($response->registrationStartFrom));
 			  ?>
