9be61ae42153980bf53d12c763492979d480dac8
prakash thakur <prakashthakur@crescerance.com>
2024-02-23 14:05:14 +0530

    position and depart,ents in dashboard

--- wp-content/plugins/engagifii/includes/public/views/dashboard/engagifii-profile.php
+++ wp-content/plugins/engagifii/includes/public/views/dashboard/engagifii-profile.php
@@ -104,19 +104,38 @@ include 'sidebar_nav.php';
               <span class="mr-4 bg-white rounded py-1 px-2 d-none"><strong>Status:</strong> <span style="color:<?php echo $statusColor; ?>;"><?php echo $status; ?></span></span>
               <?php } 
 			  $dp = json_decode($peopleDATA->tabs[2]->groupFields[2]->fields[0]->selectedValue, true);
-			  if(count($dp[0]['positionHistory']>0)){ ?>
-                  <div class="dropdown">
-                  <a class="" href="" data-toggle="dropdown" aria-expanded="false">
-                    <?php echo count($dp[0]['positionHistory']). ' Positions'; ?>
-                  </a>
-                  <div class="dropdown-menu py-1">
-                  	<h6 class="bg-light text-center py-1 mb-1">Positions (<?php echo count($dp[0]['positionHistory']); ?>)</h6>
-                    <?php foreach($dp[0]['positionHistory'] as $key => $position){ 
-                        echo '<span class="dropdown-item px-2 py-0 small text-dark">'.$position['positionName'].'</span><span class="dropdown-item pr-2 py-0  pl-4 small  text-dark"><b>'.$position['departmentName'].'</b></span><div class="dropdown-divider my-1"></div>';
-                    } ?>
-                  </div>
-                </div>
-              <?php  } ?>
+			  if(count($dp[0]['positionHistory'])>0){
+					  if(count($dp[0]['positionHistory'])==1){
+						 $department =$dp[0]['positionHistory'][0]['departmentName'];
+  						echo '<span class="mr-4 bg-white rounded py-1 px-2"><strong>Department: </strong>'.$department.'</span>';
+						 $position =$dp[0]['positionHistory'][0]['positionName'];
+  						echo '<span class="mr-4 bg-white rounded py-1 px-2"><strong>Position: </strong>'.$position.'</span>';
+					  }else{ ?>
+                      	<div class="dropdown">
+                          <a class="mr-4 bg-white rounded py-1 px-2" href="" data-toggle="dropdown" aria-expanded="false">
+                            <?php echo count($dp[0]['positionHistory']). ' Departments'; ?>
+                          </a>
+                          <div class="dropdown-menu py-1">
+                            <h6 class="bg-light text-center py-1 mb-1">Departments (<?php echo count($dp[0]['positionHistory']); ?>)</h6>
+						<?php  foreach($dp[0]['positionHistory'] as $key => $position){ 
+                       		 echo '<span class="dropdown-item px-2 py-0 small text-dark">'.$position['departmentName'].'</span>';
+                   		 }  ?>
+                         </div>
+               			 </div> 
+                      	<div class="dropdown">
+                          <a class="mr-4 bg-white rounded py-1 px-2" href="" data-toggle="dropdown" aria-expanded="false">
+                            <?php echo count($dp[0]['positionHistory']). ' Positions'; ?>
+                          </a>
+                          <div class="dropdown-menu py-1">
+                            <h6 class="bg-light text-center py-1 mb-1">Positions (<?php echo count($dp[0]['positionHistory']); ?>)</h6>
+						<?php  foreach($dp[0]['positionHistory'] as $key => $position){ 
+                       		 echo '<span class="dropdown-item px-2 py-0 small text-dark">'.$position['positionName'].'</span>';
+                   		 }  ?>
+                         </div>
+               			 </div> 
+					 <?php  }
+				  
+           } ?>
   </div>
         </div>
         </div>
