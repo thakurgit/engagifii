29b4b25002054a60d4f5f553d86204380bdcdf28
Guru0777 <gurpreetsingh@crescerance.com>
2024-02-27 15:14:16 +0530

    session placing changed

--- wp-content/plugins/engagifii/includes/public/views/dashboard/my-transcript.php
+++ wp-content/plugins/engagifii/includes/public/views/dashboard/my-transcript.php
@@ -1,10 +1,11 @@
-<?php session_start();
+<?php 
 if (! is_user_logged_in()) {
     echo "<br><br><div class='alert alert-warning' role='alert'><h5 class='text-center'>";
     printf(esc_attr('This page is restricted. Please %s to view this page.', 'wpfep'), wp_loginout('', false));
     echo '</h5></div>';
     return;
 }
+session_start();
 $pid = $_SESSION['pid'];
 $obj      =  new Engagifii_API();
 $user_id  = get_current_user_id();
