962e5e781fe8cbb40e56087a0c21b47daa57b85b
Guru0777 <gurpreetsingh@crescerance.com>
2024-02-27 16:30:02 +0530

    remove 0 from time

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -2774,12 +2774,14 @@ wp_die();
                 $convert_Date = strtotime($default_Date);
                 $startdate = date('M d, Y', $convert_Date);
                 $starttime = date('h:i A', $convert_Date);
+                $starttime = ltrim($starttime, '0');
             }
             if($row->endDateTime){
                 $default_Date = $row->endDateTime;
                 $convert_Date = strtotime($default_Date);
                 $enddate = date('M d, Y', $convert_Date);
                 $endtime = date('h:i A', $convert_Date);
+                $endtime = ltrim($endtime, '0');
             }
             $nestedData['startDateTime'] ='<span style="display:none;">'.strtotime($startdate).'</span>'. $startdate." at ".$starttime." - ".$enddate." at ".$endtime ;
             
