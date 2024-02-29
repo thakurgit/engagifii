c3fec525ecbb4aa97cafc94f782b90f852c5a9bf
Guru0777 <gurpreetsingh@crescerance.com>
2024-02-29 18:55:38 +0530

    UI changes

--- wp-content/plugins/engagifii/includes/public/views/dashboard/my-transcript.php
+++ wp-content/plugins/engagifii/includes/public/views/dashboard/my-transcript.php
@@ -206,9 +206,9 @@ z-index:-1;
   </div>
   <div class="tab-pane p-3 fade" id="credits" role="tabpanel" aria-labelledby="profile-tab">
   
-  <div class="d-flex justify-content-between align-items-center mb-3">
+  <div class="d-flex align-items-center mb-3">
    <button type="button" title="Refresh Downloads" class="refresh btn shadow-none p-2"> <i class="fas fa-sync"></i></button><?php echo '<img src="'.ENGAGIFII_ASSETS_URL.'/images/class.png" class="img-icon-lg img-fluid mr-2" alt="class-icon" >'; ?>
-  	<strong><p class="mr-2 mb-1" style="font-size: 10px; color:#2568ef;">Training Credits Earned <br>Within a Date Range</p></strong>
+  	<strong><p class="mr-2 mb-1" style="font-size: 14px; color:#2568ef; line-hieght:1;">Training Credits Earned <br>Within a Date Range</p></strong>
     <div class="form-inline dateFilter">
     <div class="input-group mr-2" style="max-width:255px">
     <input type="text" class="form-control form-control-sm shadow-none" placeholder="Select Date Range">
