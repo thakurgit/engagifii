ebe7c4a62fe768f0406099db95838d2704d137d4
Guru0777 <gurpreetsingh@crescerance.com>
2024-03-05 18:28:56 +0530

    Header UI changes

--- wp-content/plugins/engagifii/includes/public/views/dashboard/engagifii-profile.php
+++ wp-content/plugins/engagifii/includes/public/views/dashboard/engagifii-profile.php
@@ -115,13 +115,13 @@ include 'sidebar_nav.php';
 			  if(count($dp[0]['positionHistory'])>0){
 					  if(count($dp[0]['positionHistory'])==1){
 						 $department =$dp[0]['positionHistory'][0]['departmentName'];
-  						echo '<span class="py-1 px-2"><strong>Department: </strong>'.$department.'</span> | ';
+  						echo '<span class="py-1 px-2"><strong>Department: </strong>'.$department.'</span>';
 						 $position =$dp[0]['positionHistory'][0]['positionName'];
   						echo '<span class="py-1 px-2"><strong>Position: </strong>'.$position.'</span>';
 					  }else{ ?>
                       	<div class="dropdown">
-                          <a class="mr-4 bg-white rounded py-1 px-2" href="" data-toggle="dropdown" aria-expanded="false">
-                            <?php echo count($dp[0]['positionHistory']). ' Departments'; ?>
+                          <a class="py-1 px-2" href="" data-toggle="dropdown" aria-expanded="false">
+                            <?php echo count($dp[0]['positionHistory']). ' Departments'; ?> | 
                           </a>
                           <div class="dropdown-menu py-1">
                             <h6 class="bg-light text-center py-1 mb-1">Departments (<?php echo count($dp[0]['positionHistory']); ?>)</h6>
@@ -131,7 +131,7 @@ include 'sidebar_nav.php';
                          </div>
                			 </div> 
                       	<div class="dropdown">
-                          <a class="mr-4 bg-white rounded py-1 px-2" href="" data-toggle="dropdown" aria-expanded="false">
+                          <a class="py-1 px-2" href="" data-toggle="dropdown" aria-expanded="false">
                             <?php echo count($dp[0]['positionHistory']). ' Positions'; ?>
                           </a>
                           <div class="dropdown-menu py-1">
