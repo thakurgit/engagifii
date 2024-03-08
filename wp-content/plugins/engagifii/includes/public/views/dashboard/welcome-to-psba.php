9171d6ed2167df90469886f4b342caf0d5acd9a8
prakash thakur <prakashthakur@crescerance.com>
2024-03-08 10:50:01 +0530

    psba welcome

--- wp-content/plugins/engagifii/includes/public/views/dashboard/welcome-to-psba.php
+++ wp-content/plugins/engagifii/includes/public/views/dashboard/welcome-to-psba.php
@@ -1,1 +1,10 @@
+<?php 
+if (! is_user_logged_in()) {
+    echo "<br><br><div class='alert alert-warning' role='alert'><h5 class='text-center'>";
+    printf(esc_attr('This page is restricted. Please %s to view this page.', 'wpfep'), wp_loginout('', false));
+    echo '</h5></div>';
+    return;
+}
+include 'sidebar_nav.php';
+?>
 <h2>Welcome to MyPSBA </h2>
