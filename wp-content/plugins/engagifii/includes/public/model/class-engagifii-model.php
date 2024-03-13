511665eccefe3a02c0c9c73cd2883fbb4fc59c5a
prakash thakur <prakashthakur@crescerance.com>
2024-03-13 13:17:27 +0530

    event filter async

--- wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
+++ wp-content/plugins/engagifii/includes/public/model/class-engagifii-model.php
@@ -2698,7 +2698,7 @@ public function eventFilters(){
 	  $filterParams = $_POST['filterParams'];
 	  $apiUrl='';
 	  $date = date('Y-m-d');
-	  foreach ($filterParams as $key => $values) {
+	  foreach ($filterParams as $keys => $values) {
 		  if($values =='startDateTime'){
 			$apiUrl='event/GetMinMaxEventDate/'.$date; 
 		  }else if($values =='tags'){
@@ -2711,23 +2711,23 @@ public function eventFilters(){
 		  $response =  $this->submitApiRequest($apiUrl, $postData, 'GET', 'event');
 		  if($response['api_response']){
 			$response = json_decode($response['api_response'], true);
-			$html ='';
+			$html[$values] ='';
 			foreach ($response as $key => $value) {
 				if($values=='startDateTime'){
-					$html.='<input type="text" name="createdbetween"  class="form-control form-control-sm input-xs small-css bg-light" data-date-format="mm/dd/yyyy" placeholder="MM/DD/YYYY" >
+					$html[$values]='<input type="text" name="createdbetween"  class="form-control form-control-sm input-xs small-css bg-light" data-date-format="mm/dd/yyyy" placeholder="MM/DD/YYYY" >
           <span style="right:0; top:0; cursor:pointer" class="position-absolute cleardate mt-1 mr-2"><i class="fal fa-times"></i></span>';		
 				}else if($values =='tags'){
-					$html.='<li class="d-flex align-items-start"><input id="tag_'.$key.'" class="mr-2 mt-1" type="checkbox" name="eventsTags[]" value="'.$value['id'].'"> <label class="" for="tag_'.$key.'"><small> '.addslashes($value['name']).'</small></label></li>';	
+					$html[$values].='<li class="d-flex align-items-start"><input id="tag_'.$key.'" class="mr-2 mt-1" type="checkbox" name="eventsTags[]" value="'.$value['id'].'"> <label class="" for="tag_'.$key.'"><small> '.addslashes($value['name']).'</small></label></li>';	
 				}else if($values =='city'){
-					$html.= '<li class="d-flex align-items-start"><input  type="checkbox" name="eventsLocation[]" id="location_'.$key.'" value="'.$value['id'].'" class="mr-2 mt-1"> <label for="location_'.$key.'"><small>'.addslashes($value['city']).'</small></label></li>';	
+					$html[$values].= '<li class="d-flex align-items-start"><input  type="checkbox" name="eventsLocation[]" id="location_'.$key.'" value="'.$value['id'].'" class="mr-2 mt-1"> <label for="location_'.$key.'"><small>'.addslashes($value['city']).'</small></label></li>';	
 				}else if($values=='eventType'){
-				  $html.= '<li class="d-flex align-items-start"><input type="checkbox" name="eventsType[]" id="event_'.$key.'" value="'.$value['value'].'" class="mr-2 mt-1"> <label for="event_'.$key.'"><small> '.addslashes($value['text']).'</small></label></li>';
+				  $html[$values].= '<li class="d-flex align-items-start"><input type="checkbox" name="eventsType[]" id="event_'.$key.'" value="'.$value['value'].'" class="mr-2 mt-1"> <label for="event_'.$key.'"><small> '.addslashes($value['text']).'</small></label></li>';
 				}
 			  }
 	  		} else {
-				$html='<h6 class="text-center mt-3">data not found</h6>';	
+				$html[$values]='<h6 class="text-center mt-3">data not found</h6>';	
 			}
-			$htmlArray[]=$html;
+			$htmlArray=$html;
 	  }
 		echo json_encode($htmlArray);
         wp_die();
