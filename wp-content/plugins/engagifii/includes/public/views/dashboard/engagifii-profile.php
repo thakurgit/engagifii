6697ab9c8ad0158614491953b016fcdd8de0ce1f
Guru0777 <gurpreetsingh@crescerance.com>
2024-04-04 17:29:36 +0530

    set cookies name and dp

--- wp-content/plugins/engagifii/includes/public/views/dashboard/engagifii-profile.php
+++ wp-content/plugins/engagifii/includes/public/views/dashboard/engagifii-profile.php
@@ -63,10 +63,10 @@ echo "<br><br><div class='alert alert-danger' role='alert'><h5 class='text-cente
 	$_SESSION['pid']=$peopleDATA->people->id;
   setcookie('pid', $peopleDATA->people->id, time() + (24 * 3600), '/'); 
 
-$loggedin_username = "test";//$peopleDATA->people->firstName.' '.$peopleDATA->people->middleName.' '.$peopleDATA->people->lastName;
-$loggedin_userdp = "dp";//$peopleDATA->people->imageThumbUrl;
-setcookie('loggedin_username', $loggedin_username, time() + (86400 * 30), "/");
-setcookie('loggedin_userdp', $loggedin_userdp, time() + (86400 * 30), "/"); // 86400 = 1 day
+$loggedin_username = $peopleDATA->people->firstName.' '.$peopleDATA->people->middleName.' '.$peopleDATA->people->lastName;
+$loggedin_userdp = $peopleDATA->people->imageThumbUrl;
+setcookie('loggedin_username', $loggedin_username, time() + (24 * 3600), "/");
+setcookie('loggedin_userdp', $loggedin_userdp, time() + (24 * 3600), "/"); // 86400 = 1 day
   
     $_SESSION['name']=$peopleDATA->people->firstName.' '.$peopleDATA->people->middleName.' '.$peopleDATA->people->lastName;
     $_SESSION['dp']=$peopleDATA->people->imageThumbUrl;
