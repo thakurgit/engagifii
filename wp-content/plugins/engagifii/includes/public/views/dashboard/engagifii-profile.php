9c049e50b834a52c143212fcc43dcf9cafb129ff
Guru0777 <gurpreetsingh@crescerance.com>
2024-04-10 15:18:45 +0530

    edit profile broken image fix push

--- wp-content/plugins/engagifii/includes/public/views/dashboard/engagifii-profile.php
+++ wp-content/plugins/engagifii/includes/public/views/dashboard/engagifii-profile.php
@@ -325,7 +325,7 @@ foreach ($peopleDATA->peopleFields as $key => $value) {
     <?php } else { ?>
     	<i class="fa fa-user text-secondary" style="font-size:110px"></i>
     <?php } ?>
-    <!-- <img src="<?php echo $peopleDATA->people->imageThumbUrl; ?>" alt="..." class="img-fluid h-100" id="blah"  > -->
+    <!-- <img src="<?php //echo $peopleDATA->people->imageThumbUrl; ?>" alt="..." class="img-fluid h-100" id="blah"  > -->
     <span class="position-absolute w-100 h-100 top-0 start-0 text-white d-flex align-items-center flex-column justify-content-center" style="background:rgba(0,0,0,0.6); opacity:0; top:0; left:0"><i class="fa fa-image"></i><br>Upload</span>
     <input type="file" class="position-absolute top-0 start-0 w-100 h-100 z-1" style="opacity:0; top:0; left:0" accept="image/*" id="imgInp" onchange=""> 
     <style>
