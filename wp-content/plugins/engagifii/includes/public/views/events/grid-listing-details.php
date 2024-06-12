6e7199ee6f01ef88badfb0910a7f0b9b1e0ed4c0
Guru0777 <gurpreetsingh@crescerance.com>
2024-06-12 15:28:51 +0530

    condition applied on google map - event detail page

--- wp-content/plugins/engagifii/includes/public/views/events/grid-listing-details.php
+++ wp-content/plugins/engagifii/includes/public/views/events/grid-listing-details.php
@@ -339,7 +339,9 @@ if ( strpos($url,'my-profile') !== false ) {
                                                     <li><strong>Address: </strong>
 												<?php echo $value->addressLine." ".$value->city." ".$value->state." ".$value->zip; ?>, <?php echo $value->country; ?></li>
 												<?php } 
+												if($value->latitude && $value->longitude){
 												echo '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://maps.google.com/maps?q='.$value->latitude.','.$value->longitude.'&hl=en&z=14&amp;output=embed" allowfullscreen></iframe></div></div>';
+												}
 												?>
 												
                                                 </ul>
