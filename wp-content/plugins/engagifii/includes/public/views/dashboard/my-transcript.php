fb0bbf49e83ec300a10af837afb0c1ba530523d3
Guru0777 <gurpreetsingh@crescerance.com>
2024-06-13 13:07:39 +0530

    Timezone set to Central Standard Time

--- wp-content/plugins/engagifii/includes/public/views/dashboard/my-transcript.php
+++ wp-content/plugins/engagifii/includes/public/views/dashboard/my-transcript.php
@@ -1,5 +1,6 @@
 <?php 
 ini_set('session.gc_maxlifetime', 3600);
+date_default_timezone_set('America/Chicago');
 if (! is_user_logged_in()) {
 	$login = 'moOAuthLoginNew("Engagifii")';
     echo "<br><br><div class='alert alert-warning' role='alert'><h5 class='text-center mb-0'>This page is restricted. Please";
