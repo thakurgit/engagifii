307d1e39f28924ded7cab6b079878269fe010eda
Guru0777 <gurpreetsingh@crescerance.com>
2024-08-02 16:04:24 +0530

    clear cookies before signin

--- wp-content/plugins/engagifii/includes/functions.php
+++ wp-content/plugins/engagifii/includes/functions.php
@@ -29,6 +29,16 @@ function clearAllCookies(newtab='') {
 	}
 	
 }
+function clearCookies() {
+	  localStorage.clear();  
+     var cookies = document.cookie.split(";");
+   for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+	}
 
 		function moOAuthLoginNew(app_name) {
 			
@@ -133,7 +143,7 @@ function add_loginout_link( $items, $args ) {
         $items .= '<li class="nav-item"><a title="Logout" class="nav-link login-btn" onclick="clearAllCookies()" target="_blank" href="'. wp_logout_url() .'">Log Out</a></li>';
     }
     elseif (!is_user_logged_in() && $args->theme_location == 'primary' && $login_btn ) {
-        $items .= '<li class="nav-item"><a onClick="'.$login.'" class="nav-link login-btn" title="Login with Engagifii" href="javascript:void">Log In</a></li>';
+        $items .= '<li class="nav-item"><a onClick="clearCookies(); '.$login.'" class="nav-link login-btn" title="Login with Engagifii" href="javascript:void">Log In</a></li>';
     }
     return $items;
 }
