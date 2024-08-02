4b2fb0576219b42a1c6050d15f0849f30f949b09
Guru0777 <gurpreetsingh@crescerance.com>
2024-08-02 21:36:31 +0530

    custom logout function code revert

--- wp-content/plugins/engagifii/includes/functions.php
+++ wp-content/plugins/engagifii/includes/functions.php
@@ -39,22 +39,7 @@ function clearCookies() {
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
     }
 	}
-
-    // Hook into the logout action to handle additional logout processes
-function custom_logout_redirect() {
-    wp_redirect('https://engagifii-qa-identity.azurewebsites.net/Account/SignOut?ReturnUrl=https://engagifiwebstg.wpengine.com/psba/');
-    exit();
-}
-add_action('wp_logout', 'custom_logout_redirect');
-
-// Customize the logout URL to include the custom redirect URL
-function custom_logout_url( $logout_url, $redirect ) {
-    $custom_redirect = 'https://engagifii-qa-identity.azurewebsites.net/Account/SignOut?ReturnUrl=https://engagifiwebstg.wpengine.com/psba/';
-    return add_query_arg('redirect_to', urlencode($custom_redirect), $logout_url);
-}
-add_filter('logout_url', 'custom_logout_url', 10, 2);
-
-
+    
 
 		function moOAuthLoginNew(app_name) {
 			
