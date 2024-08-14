66be83eca4d1e895f428c76f8c10aa420c23d673
prakash thakur <prakashthakur@crescerance.com>
2024-08-14 14:07:45 +0530

    remove est

--- wp-content/plugins/engagifii/includes/admin/view/endorsement/admin-column-list.php
+++ wp-content/plugins/engagifii/includes/admin/view/endorsement/admin-column-list.php
@@ -3,9 +3,6 @@
 <?php
     $obj =  new adminDataColumn();
     $response = $obj->getColumnData();
-	if(!$response){
-		echo 'est';
-	}
     $options = get_option( 'ebt_api_settings' );
     $ebt_visib_datacol_list = array();
     if(isset($options['ebt_visib_datacol_list'])){
