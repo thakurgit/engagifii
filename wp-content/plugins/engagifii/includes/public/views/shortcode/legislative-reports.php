bee9c47a0d0c3016bc3fe561400ec6561592feca
guru0777 <gurpreetsingh@crescerance.com>
2026-01-12 11:43:18 +0530

    debug code

--- wp-content/plugins/engagifii/includes/public/views/shortcode/legislative-reports.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/legislative-reports.php
@@ -13,6 +13,7 @@ if (filter_var($tenant_url, FILTER_VALIDATE_URL)) {
 //echo $tenant; // Output will be "gsba" in both cases
 
 $reportsResponse = $obj->legislativeReports();
+print
 if(!$reportsResponse['api_response']){
 		echo'<h5 class="text-center pt-5">Data not available</h5>';
 		return;	
