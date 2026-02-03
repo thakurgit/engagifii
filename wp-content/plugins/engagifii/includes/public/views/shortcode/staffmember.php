f95802d653f6237648e76e9d77c3c677d3cb1ff7
guru0777 <gurpreetsingh@crescerance.com>
2026-02-03 17:05:21 +0530

    count related changes

--- wp-content/plugins/engagifii/includes/public/views/shortcode/staffmember.php
+++ wp-content/plugins/engagifii/includes/public/views/shortcode/staffmember.php
@@ -110,7 +110,7 @@ function getStaffMembers(sessionId)
 			
 			$.each(data, function(i, item) {
 				if($.inArray(item.personId, allmembers) != -1) {
-					if(item.count>0){
+					if(item.count>-1){
 						if(sessionId==0){
 						  html += '<a href="<?php echo BILLS_PAGE_LINK; ?>?member='+item.personId+'&'+btoa(item.fullName)+'" class="list-group-item list-group-item-action py-1 px-2 border-0">'+item.fullName+' ('+item.count+')</a>';
 						}else{
